fee6978139a022cec25065cc57150838c115bf85
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2014-07-14 10:47:37 +0000

    added header
    git-svn-id: https://zerberus.eonerc.rwth-aachen.de:8443/svn/s2ss/trunk@145 8ec27952-4edc-4aab-86aa-e87bb2611832

--- clients/opal/models/AsyncIP_sl/AsyncIP.c
+++ clients/opal/models/AsyncIP_sl/AsyncIP.c
@@ -1,21 +1,16 @@
-/*-------------------------------------------------------------------
- *  OPAL-RT Technologies inc
- *
- *  Copyright (C) 2003. All rights reserved.
- *
- *  File name =         AsyncIP.c
- *  Last modified by =  Mathieu Dubé-Dallaire
+/** AsyncIP
  *
  *  Code example of an asynchronous program.  This program is started
  *  by the asynchronous controller and demonstrates how to send and 
  *  receive data to and from the asynchronous icons and a UDP or TCP
  *  port.
  *
- *  Feel free to use this as a starting point for your own asynchronous
- *  application. You should normally only have to modify the sections
- *  marked with: ****** Format to specific protocol here. ******.
- *
- *-----------------------------------------------------------------*/
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @author Mathieu Dubé-Dallaire
+ * @copyright 2014, Institute for Automation of Complex Power Systems, EONERC
+ * @copyright 2003, OPAL-RT Technologies inc
+ * @file
+ */
 
 #define PROGNAME "AsyncIP"
 
