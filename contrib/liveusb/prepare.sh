c75e03db7752a872f327ab9de2558abb02a50b3f
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2015-09-27 16:04:15 +0200

    moved some stuff to InetDistSim repository because it does not belong to S2SS

--- contrib/liveusb/prepare.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rpm -Uvh http://ccrma.stanford.edu/planetccrma/mirror/fedora/linux/planetccrma/20/i386/planetccrma-repo-1.1-3.fc20.ccrma.noarch.rpm 
-
-yum update
-
-yum install planetccrma-core
-
-source update_boot.sh
