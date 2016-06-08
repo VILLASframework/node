3e0c743d80304cfeb29a79a4860da53a44f312ae
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2016-06-08 22:38:50 +0200

    adapting hook feature to new data structures

--- include/hooks.h
+++ include/hooks.h
@@ -23,10 +23,20 @@
 
 #include <time.h>
 
-#define REGISTER_HOOK(name, prio, fnc, type)				\
-__attribute__((constructor)) void __register_ ## fnc () {		\
-	static struct hook h = { name, NULL, prio, type, NULL, fnc };	\
-	list_push(&hooks, &h);						\
+#include "queue.h"
+#include "list.h"
+
+#define REGISTER_HOOK(nam, desc, prio, hist, fnc, typ)		\
+__attribute__((constructor)) void __register_ ## fnc () {	\
+	static struct hook h = {				\
+		.name = nam,					\
+		.description = desc,				\
+		.priority = prio,				\
+		.history = hist,				\
+		.type = typ,					\
+		.cb = fnc					\
+	};							\
+	list_push(&hooks, &h);					\
 }
 
 /* The configuration of hook parameters is done in "config.h" */
@@ -34,6 +44,8 @@ __attribute__((constructor)) void __register_ ## fnc () {		\
 /* Forward declarations */
 struct path;
 struct hook;
+struct sample;
+struct settings;
 
 /** This is a list of hooks which can be used in the configuration file. */
 extern struct list hooks;
@@ -42,34 +54,40 @@ extern struct list hooks;
  *
  * @param p The path which is processing this message.
  * @param h The hook datastructure which contains parameter, name and private context for the hook.
+ * @param m A pointer to the first message which should be processed by the hook.
+ * @param cnt The number of messages which should be processed by the hook.
  * @param when Provides the type of hook for which this occurence of the callback function was executed. See hook_type for possible values.
  * @retval 0 Success. Continue processing and forwarding the message.
  * @retval <0 Error. Drop the message.
  */
-typedef int (*hook_cb_t)(struct path *p, struct hook *h, int when);
+typedef int (*hook_cb_t)(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
 
 /** The type of a hook defines when a hook will be exectuted. This is used as a bitmask. */
 enum hook_type {
-	HOOK_PATH_START		= 1 << 0, /**< Called whenever a path is started; before threads are created. */
-	HOOK_PATH_STOP		= 1 << 1, /**< Called whenever a path is stopped; after threads are destoyed. */
-	HOOK_PATH_RESTART	= 1 << 2, /**< Called whenever a new simulation case is started. This is detected by a sequence no equal to zero. */
+	HOOK_PATH_START		= 1 << 0,  /**< Called whenever a path is started; before threads are created. */
+	HOOK_PATH_STOP		= 1 << 1,  /**< Called whenever a path is stopped; after threads are destoyed. */
+	HOOK_PATH_RESTART	= 1 << 2,  /**< Called whenever a new simulation case is started. This is detected by a sequence no equal to zero. */
 
-	HOOK_PRE		= 1 << 3, /**< Called when a new packet of messages (samples) was received. */
-	HOOK_POST		= 1 << 4, /**< Called after each message (sample) of a packet was processed. */
-	HOOK_MSG		= 1 << 5, /**< Called for each message (sample) in a packet. */
-	HOOK_ASYNC		= 1 << 6, /**< Called asynchronously with fixed rate (see path::rate). */
-	
-	HOOK_PERIODIC		= 1 << 7, /**< Called periodically. Period is set by global 'stats' option in the configuration file. */
-	
+	HOOK_READ		= 1 << 3,  /**< Called for every single received samples. */
+	HOOK_WRITE		= 1 << 4,  /**< Called for every single sample which will be sent. */
+
+	HOOK_ASYNC		= 1 << 7,  /**< Called asynchronously with fixed rate (see path::rate). */
+	HOOK_PERIODIC		= 1 << 8,  /**< Called periodically. Period is set by global 'stats' option in the configuration file. */
+
+	HOOK_INIT		= 1 << 9,  /**< Called before path is started to parse parameters. */
+	HOOK_DEINIT		= 1 << 10, /**< Called after path has been stopped to release memory allocated by HOOK_INIT */	
+
+	HOOK_INTERNAL		= 1 << 11,  /**< Internal hooks are added to every path implicitely. */
+	HOOK_PARSE		= 1 << 12, /**< Called for parsing hook arguments. */
+
 	/** @{ Classes of hooks */
-	/** Internal hooks are mandatory. */
-	HOOK_INTERNAL		= 1 << 16,
 	/** Hooks which are using private data must allocate and free them propery. */	
-	HOOK_PRIVATE		= HOOK_PATH_START | HOOK_PATH_STOP,
+	HOOK_STORAGE		= HOOK_INIT | HOOK_DEINIT,
 	/** All path related actions */
 	HOOK_PATH		= HOOK_PATH_START | HOOK_PATH_STOP | HOOK_PATH_RESTART,
 	/** Hooks which are used to collect statistics. */
-	HOOK_STATS		= HOOK_INTERNAL | HOOK_PRIVATE | HOOK_PATH | HOOK_MSG | HOOK_PRE | HOOK_PERIODIC,
+	HOOK_STATS		= HOOK_INTERNAL | HOOK_STORAGE | HOOK_PATH | HOOK_READ | HOOK_PERIODIC,
+
 	/** All hooks */
 	HOOK_ALL		= HOOK_INTERNAL - 1
 	/** @} */
@@ -79,9 +97,19 @@ enum hook_type {
 struct hook {
 	const char *name;	/**< The unique name of this hook. This must be the first member! */
 	const char *parameter;	/**< A parameter string for this hook. Can be used to configure the hook behaviour. */
+	const char *description;/**< A short description of this hook function. */
+
 	int priority;		/**< A priority to change the order of execution within one type of hook */
+	int history;		/**< How many samples of history this hook requires. */
 	enum hook_type type;	/**< The type of the hook as a bitfield */
-	void *private;		/**< Private data for this hook. This pointer can be used to pass data between consecutive calls of the callback. */
+
+	void *_vd;		/**< Private data for this hook. This pointer can be used to pass data between consecutive calls of the callback. */
+
+	struct sample *last;
+	struct sample *prev;
+	
+	qptr_t head;
+
 	hook_cb_t cb;		/**< The hook callback function as a function pointer. */
 };
 
@@ -94,60 +122,37 @@ int hooks_sort_priority(const void *a, const void *b);
 /** Conditionally execute the hooks
  *
  * @param p A pointer to the path structure.
- * @param t Which type of hooks should be executed?
+ * @param when Which type of hooks should be executed?
+ * @param m An array to of (cnt) pointers to msgs.
+ * @param cnt The size of the message array.
  * @retval 0 All registred hooks for the specified type have been executed successfully. 
  * @retval <0 On of the hook functions signalized, that the processing should be aborted; message should be skipped.
  */
-int hook_run(struct path *p, enum hook_type t);
+int hook_run(struct path *p, struct sample *smps[], size_t cnt, int when);
 
-/** The following prototypes are example hooks
+/** Allocate & deallocate private memory per hook.
  *
- * @addtogroup hooks_examples Examples for hook functions
- * @{
+ * Hooks which use this function must be flagged with HOOL_STORAGE.
+ *
+ * @param h A pointer to the hook structure.
+ * @param when Which event cause the hook to be executed?
+ * @param len The size of hook prvate memory allocation.
+ * @return A pointer to the allocated memory region or NULL after it was released.
  */
+void * hook_storage(struct hook *h, int when, size_t len);
 
-/** Example hook: Print the message. */
-int hook_print(struct path *p, struct hook *h, int when);
+int hook_print(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_ts(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_convert(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_decimate(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_skip_first(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
 
-/** Example hook: Drop messages. */
-int hook_decimate(struct path *p, struct hook *h, int when);
-
-/** Example hook: Convert the values of a message between fixed (integer) and floating point representation. */
-int hook_convert(struct path *p, struct hook *h, int when);
-
-/** Example hook: overwrite timestamp of message. */
-int hook_ts(struct path *p, struct hook *h, int when);
-
-/** Internal hook: add missing timestamps for node types which do not include a valid TS (ex. GTFPGA) */
-int hook_fix_ts(struct path *p, struct hook *h, int when);
-
-/** Example hook: Finite-Impulse-Response (FIR) filter. */
-int hook_fir(struct path *p, struct hook *h, int when);
-
-/** Example hook: drop first samples after simulation restart */
-int hook_skip_first(struct path *p, struct hook *h, int when);
-
-/** Example hook: Skip messages whose values are similiar to the previous ones */
-int hook_skip_unchanged(struct path *p, struct hook *h, int when);
-
-/* The following hooks are used to implement core functionality */
-
-/** Core hook: verify message headers. Invalid messages will be dropped. */
-int hook_verify(struct path *p, struct hook *h, int when);
-
-/** Core hook: reset the path in case a new simulation was started. */
-int hook_restart(struct path *p, struct hook *h, int when);
-
-/** Core hook: check if sequence number is correct. Otherwise message will be dropped */
-int hook_drop(struct path *p, struct hook *h, int when);
-
-/** Core hook: collect statistics */
-int hook_stats(struct path *p, struct hook *h, int when);
-
-/** Core hook: send path statistics to another node */
-int hook_stats_send(struct path *p, struct hook *h, int when);
-
-/** Not a hook: just prints header for periodic statistics */
+int hook_stats_send(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_stats(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
 void hook_stats_header();
 
-#endif /** _HOOKS_H_ @} @} */
+int hook_fix_ts(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_restart(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+int hook_drop(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt);
+
+#endif /** _HOOKS_H_ @} */
---
+++ lib/hooks-stats.c
@@ -0,0 +1,147 @@
+/** Statistic-related hook functions.
+ *
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @copyright 2014-2016, Institute for Automation of Complex Power Systems, EONERC
+ *   This file is part of S2SS. All Rights Reserved. Proprietary and confidential.
+ *   Unauthorized copying of this file, via any medium is strictly prohibited.
+ *********************************************************************************/
+
+#include "hooks.h"
+#include "sample.h"
+#include "path.h"
+#include "utils.h"
+#include "timing.h"
+
+extern struct list *hook_nodes;
+
+void hook_stats_header()
+{
+	#define UNIT(u)	"(" YEL(u) ")"
+
+	stats("%-40s|%19s|%19s|%19s|%19s|%19s|%19s|%10s|", "Source " MAG("=>") " Destination",
+		"OWD"	UNIT("S") " ",
+		"Rate"	UNIT("p/S") " ",
+		"Recv"	UNIT("p") " ",
+		"Drop"	UNIT("p") " ",
+		"Skip"	UNIT("p") " ",
+		"Inval" UNIT("p") " ",
+		"Overuns "
+	);
+	line();
+}
+
+REGISTER_HOOK("stats", "Collect statistics for the current path", 2, 1, hook_stats, HOOK_STATS)
+int hook_stats(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt)
+{
+	switch (when) {
+		case HOOK_INIT:
+			/** @todo Allow configurable bounds for histograms */
+			hist_create(&p->hist.owd,      0,         1,         100e-3);
+			hist_create(&p->hist.gap_msg,  90e-3,     110e-3,      1e-3);
+			hist_create(&p->hist.gap_recv, 90e-3,     110e-3,      1e-3);
+			hist_create(&p->hist.gap_seq,  -HIST_SEQ, +HIST_SEQ,   1);
+			break;
+
+		case HOOK_READ:
+			for (int i = 0; i < cnt; i++) {
+				h->last  = smps[i];
+				
+				if (h->prev) {
+					int    gap_seq  = h->last->sequence - (int32_t) h->prev->sequence;
+					double owd      = time_delta(&h->last->ts.origin, &h->last->ts.received);
+					double gap      = time_delta(&h->prev->ts.origin, &h->last->ts.origin);
+					double gap_recv = time_delta(&h->prev->ts.received, &h->last->ts.received);
+
+					hist_put(&p->hist.gap_msg, gap);
+					hist_put(&p->hist.gap_seq, gap_seq);
+					hist_put(&p->hist.owd, owd);
+					hist_put(&p->hist.gap_recv, gap_recv);
+				}
+
+				h->prev = h->last;
+			}
+			break;
+
+		case HOOK_PATH_STOP:
+			if (p->hist.owd.total)     { info("One-way delay:"); hist_print(&p->hist.owd); }
+			if (p->hist.gap_recv.total){ info("Inter-message arrival time:"); hist_print(&p->hist.gap_recv); }
+			if (p->hist.gap_msg.total) { info("Inter-message ts gap:"); hist_print(&p->hist.gap_msg); }
+			if (p->hist.gap_seq.total) { info("Inter-message sequence number gaps:"); hist_print(&p->hist.gap_seq); }
+			break;
+		
+		case HOOK_DEINIT:
+			hist_destroy(&p->hist.owd);
+			hist_destroy(&p->hist.gap_msg);
+			hist_destroy(&p->hist.gap_recv);
+			hist_destroy(&p->hist.gap_seq);
+			break;
+
+		case HOOK_PATH_RESTART:
+			hist_reset(&p->hist.owd);
+			hist_reset(&p->hist.gap_seq);
+			hist_reset(&p->hist.gap_msg);
+			hist_reset(&p->hist.gap_recv);
+			break;
+			
+		case HOOK_PERIODIC:
+			stats("%-40.40s|%10s|%10s|%10ju|%10ju|%10ju|%10ju|%10ju|", path_name(p), "", "", 
+				p->in->received, p->dropped, p->skipped, p->invalid, p->overrun);
+			break;
+	}
+	
+	return cnt;
+}
+
+REGISTER_HOOK("stats_send", "Send path statistics to another node", 99, 0, hook_stats_send, HOOK_STORAGE | HOOK_PARSE | HOOK_PERIODIC | HOOK_PATH)
+int hook_stats_send(struct path *p, struct hook *h, int when, struct sample *smps[], size_t cnt)
+{
+	struct private {
+		struct node *dest;
+		int ratio;
+	} *private = hook_storage(h, when, sizeof(*private));
+	
+	switch (when) {
+		case HOOK_PARSE:
+			if (!h->parameter)
+				error("Missing parameter for hook '%s'", h->name);
+			
+			if (!hook_nodes)
+				error("Missing reference to node list for hook '%s", h->name);
+
+			private->dest = list_lookup(hook_nodes, h->parameter);
+			if (!private->dest)
+				error("Invalid destination node '%s' for hook '%s'", h->parameter, h->name);
+
+			node_start(private->dest);
+			
+			break;
+
+		case HOOK_PERIODIC: {
+			int ret, length;
+			char buf[SAMPLE_LEN(9)];
+			struct sample *last, *smp = (struct sample *) buf;
+			
+			ret = queue_get(&p->queue, (void **) &last, p->in->received - 1);
+			if (ret == 1)
+				length = last->length;
+			else
+				length =  -1;
+			
+			smp->values[0].f = p->in->received;
+			smp->values[1].f = length;
+			smp->values[2].f = p->invalid;
+			smp->values[3].f = p->skipped;
+			smp->values[4].f = p->dropped;
+			smp->values[5].f = p->overrun;
+			smp->values[6].f =       p->hist.owd.last,
+			smp->values[7].f = 1.0 / p->hist.gap_msg.last;
+			smp->values[8].f = 1.0 / p->hist.gap_recv.last;
+			smp->length = 9;
+			
+			node_write(private->dest, &smp, 1); /* Send single message with statistics to destination node */
+			break;
+		}
+	}
+	
+	return 0;
+}
