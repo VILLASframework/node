8044cc6fa1b6c3fd1c687f9aee338b5886be9c70
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2016-10-20 09:12:42 -0400

    remove compile-time configuration

--- config.h
+++ config.h
@@ -24,9 +24,6 @@
 #define DEFAULT_VALUES		64
 #define DEFAULT_QUEUELEN	1024
 
-/** Whether or not to send / receive timestamp & sequence number as first values of payload */
-#define GTNET_SKT_HEADER	0
-
 /** Width of log output in characters */
 #define LOG_WIDTH		132
 
