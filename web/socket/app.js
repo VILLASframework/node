0a270c7aad1f3ed76ce0f1809d97e78208f4eb2d
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2017-04-06 20:53:18 +0200

    split Javascript code over multiple files

--- web/socket/app.js
+++ web/socket/app.js
@@ -216,44 +216,4 @@ function wsUrl(endpoint) {
 	return url;
 }
 
-function fileStart(e) {
-	var file = e.target.files[0];
-	var reader = new FileReader();
-	
-	var start;
-	var msgs = [ ]
-	var position = 0;
-
-	reader.onload = function(e) {
-		var lines = e.target.result.split(/[\r\n]+/g); // tolerate both Windows and Unix linebreaks
-
-		for (var i = 0; i < lines.length; i++) {
-			var msg = new Msg();
-			msg.parse(lines[i]);
-			msgs.push(msg);
-		}
-
-		console.log("Read " + msgs.length + " samples from file");
-
-		if (msgs.length > 0) {
-			var offset = Date.now() - msgs[0].ts;
-			var data = [];
-			
-			for (var i = 0; i < msgs.length; i++)
-				data.push(msgs[i].ts + offset, msgs[i].data[0]);
-		
-			plotData.push(data);
-		}
-		else {
-			
-		}
-	};
-
-	reader.readAsText(file);
-}
-
 /* Some helpers */
-function getParameterByName(name) {
-	var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
---
+++ web/socket/utils.js
@@ -0,0 +1,17 @@
+function getParameterByName(name)
+{
+	var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
+function guid()
+{
+	function s4()
+	{
+		return Math.floor((1 + Math.random()) * 0x10000)
+			.toString(16)
+			.substring(1);
+	}
+
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
