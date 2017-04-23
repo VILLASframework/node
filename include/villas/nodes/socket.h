a3222da312ca2e821d4740cdc4e1ac73a54e2d5c
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2017-04-23 21:54:24 +0200

    adapted socket node-type to support new packet format

--- include/villas/nodes/socket.h
+++ include/villas/nodes/socket.h
@@ -42,7 +42,11 @@ union sockaddr_union {
 struct socket {
 	int sd;				/**> The socket descriptor */
 	int mark;			/**> Socket mark for netem, routing and filtering */
-	int endian;			/** Endianness of the data sent/received by the node */
+	
+	enum {
+		SOCKET_ENDIAN_LITTLE,
+		SOCKET_ENDIAN_BIG
+	} endian;			/** Endianness of the data sent/received by the node */
 
 	enum socket_layer layer;	/**> The OSI / IP layer which should be used for this socket */
 	enum socket_header header;	/**> Payload header type */
