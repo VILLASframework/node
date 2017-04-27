9b1e6a33ede3d6ed3db51e73e1aa38f456720dc9
Steffen Vogel <post@steffenvogel.de>
2017-04-27 13:08:17 +0200

    add license to headers

--- web/socket/msg.js
+++ web/socket/msg.js
@@ -23,11 +23,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *********************************************************************************/
+
 /**
  * @addtogroup websocket
  * @ingroup node
  * @{
- *********************************************************************************/
+ */
 
 /* Class for parsing and printing a message */
 function Msg(c)
