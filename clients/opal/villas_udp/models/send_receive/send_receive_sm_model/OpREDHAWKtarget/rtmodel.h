d042d6ab1564a2054c7b093a44d36716e5b5b52c
Umar Farooq <ufarooq@eonerc.rwth-aachen.de>
2017-04-27 14:00:55 +0000

    opal model missing files

---
+++ clients/opal/villas_udp/models/send_receive/send_receive_sm_model/OpREDHAWKtarget/rtmodel.h
@@ -0,0 +1,24 @@
+/*
+ *  rtmodel.h:
+ *
+ * Code generation for model "send_receive_1_sm_model".
+ *
+ * Model version              : 1.449
+ * Simulink Coder version : 8.4 (R2013a) 13-Feb-2013
+ * C source code generated on : Mon Apr 24 17:35:57 2017
+ *
+ * Target selection: rtlab_rtmodel.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+#ifndef RTW_HEADER_rtmodel_h_
+#define RTW_HEADER_rtmodel_h_
+
+/*
+ *  Includes the appropriate headers when we are using rtModel
+ */
+#include "send_receive_1_sm_model.h"
+#define GRTINTERFACE                   1
+#endif                                 /* RTW_HEADER_rtmodel_h_ */
---
+++ clients/opal/villas_udp/models/send_receive/send_receive_sm_model/OpREDHAWKtarget/send_receive_1_sm_model_types.h
@@ -0,0 +1,27 @@
+/*
+ * send_receive_1_sm_model_types.h
+ *
+ * Code generation for model "send_receive_1_sm_model".
+ *
+ * Model version              : 1.449
+ * Simulink Coder version : 8.4 (R2013a) 13-Feb-2013
+ * C source code generated on : Mon Apr 24 17:35:57 2017
+ *
+ * Target selection: rtlab_rtmodel.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+#ifndef RTW_HEADER_send_receive_1_sm_model_types_h_
+#define RTW_HEADER_send_receive_1_sm_model_types_h_
+#include "rtwtypes.h"
+
+/* Parameters (auto storage) */
+typedef struct P_send_receive_1_sm_model_T_ P_send_receive_1_sm_model_T;
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_send_receive_1_sm_model_T
+  RT_MODEL_send_receive_1_sm_model_T;
+
+#endif                                 /* RTW_HEADER_send_receive_1_sm_model_types_h_ */
