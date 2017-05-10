373ac884ddbba86b7f205d2f315461b7be9de832
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2017-05-10 13:07:23 +0200

    fix sign in shift_ts hook integration test

--- tests/integration/hook-shift_ts.sh
+++ tests/integration/hook-shift_ts.sh
@@ -24,14 +24,14 @@
 
 STATS_FILE=$(mktemp)
 
-OFFSET=10
+OFFSET=-10
 EPSILON=0.05
 
 villas-signal sine -l 10 -r 10 | villas-hook shift_ts offset=${OFFSET} | villas-hook stats format=\"json\" output=\"${STATS_FILE}\" > /dev/null
 
-#jq .owd ${STATS_FILE}
+jq .owd ${STATS_FILE}
 
-jq -e ".owd.mean - ${OFFSET} | length < ${EPSILON}" ${STATS_FILE} > /dev/null
+jq -e ".owd.mean + ${OFFSET} | length < ${EPSILON}" ${STATS_FILE} > /dev/null
 
 RC=$?
 
