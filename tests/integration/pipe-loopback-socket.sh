c33f11a3d9e275686b85e5180714a7fba72529ab
Steffen Vogel <post@steffenvogel.de>
2017-06-15 15:48:25 +0200

    tests: more improvements to pipe-loopback-socket

--- tests/integration/pipe-loopback-socket.sh
+++ tests/integration/pipe-loopback-socket.sh
@@ -25,29 +25,73 @@
 CONFIG_FILE=$(mktemp)
 INPUT_FILE=$(mktemp)
 OUTPUT_FILE=$(mktemp)
-
-cat > ${CONFIG_FILE} <<- EOF
-nodes = {
-	node1 = {
-		type = "socket";
-		layer = "udp";
-
-		local = "*:12000";
-		remote = "127.0.0.1:12000"
-	}
-}
-EOF
+THEORIES=$(mktemp)
 
 # Generate test data
 villas-signal random -l 10 -n > ${INPUT_FILE}
 
+for LAYER	in udp ip eth; do
+for HEADER	in none default; do
+for ENDIAN	in big little; do
+for VERIFY_SOURCE in true false; do
+
+case ${LAYER} in
+	udp)
+		LOCAL="*:12000"
+		REMOTE="127.0.0.1:12000"
+		;;
+		
+	ip)
+		# We use IP protocol number 253 which is reserved for experimentation and testing according to RFC 3692
+		LOCAL="127.0.0.1:254"
+		REMOTE="127.0.0.1:254"
+		;;
+		
+	eth)
+		# We use IP protocol number 253 which is reserved for experimentation and testing according to RFC 7042
+		LOCAL="00:00:00:00:00:00%lo:34997"
+		REMOTE="00:00:00:00:00:00%lo:34997"
+		;;
+	esac
+
+
+cat > ${CONFIG_FILE} << EOF
+nodes = {
+	node1 = {
+		type = "socket";
+
+		layer = "${LAYER}";
+		header = "${HEADER}";
+		endian = "${ENDIAN}";
+		verify_source = ${VERIFY_SOURCE};
+
+		local = "${LOCAL}";
+		remote = "${REMOTE}";
+	}
+}
+EOF
+
 # We delay EOF of the INPUT_FILE by 1 second in order to wait for incoming data to be received
 villas-pipe ${CONFIG_FILE} node1 > ${OUTPUT_FILE} < <(cat ${INPUT_FILE}; sleep 1; echo -n)
 
-# Comapre data
+# Compare data
 villas-test-cmp ${INPUT_FILE} ${OUTPUT_FILE}
 RC=$?
 
-rm ${OUTPUT_FILE} ${INPUT_FILE} ${CONFIG_FILE}
+if (( ${RC} != 0 )); then
+	echo "=========== Sub-test failed for: layer=${LAYER}, header=${HEADER}, endian=${ENDIAN} verify_source=${VERIFY_SOURCE}"
+	cat ${CONFIG_FILE}
+	echo
+	cat ${INPUT_FILE}
+	echo
+	cat ${OUTPUT_FILE}
+	exit ${RC}
+else
+	echo "=========== Sub-test succeeded for: layer=${LAYER}, header=${HEADER}, endian=${ENDIAN} verify_source=${VERIFY_SOURCE}"
+fi
+
+done; done; done; done
+
+rm ${OUTPUT_FILE} ${INPUT_FILE} ${CONFIG_FILE} ${THEORIES}
 
 exit $RC
