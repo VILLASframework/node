1716a93b5ad2a7724c428397359b771e64fcfcb8
Steffen Vogel <post@steffenvogel.de>
2017-08-20 10:54:34 +0200

    tests: do not use conf2json anymore

--- tests/integration/api-config.sh
+++ tests/integration/api-config.sh
@@ -24,11 +24,13 @@
 
 set -e
 
-LOCAL_CONF=${SRCDIR}/etc/loopback.conf
+LOCAL_CONF=${SRCDIR}/etc/loopback.json
 
 FETCHED_CONF=$(mktemp)
 FETCHED_JSON_CONF=$(mktemp)
 LOCAL_JSON_CONF=$(mktemp)
+
+ID=$(uuidgen)
 
 # Start VILLASnode instance with local config (via advio)
 villas-node file://${LOCAL_CONF} &
@@ -37,16 +39,13 @@ villas-node file://${LOCAL_CONF} &
 sleep 1
 
 # Fetch config via API
-curl -sX POST --data '{ "action" : "config", "id" : "5a786626-fbc6-4c04-98c2-48027e68c2fa" }' http://localhost/api/v1 > ${FETCHED_CONF}
+curl -sX POST --data '{ "action" : "config", "id" : "'${ID}'" }' http://localhost/api/v1 > ${FETCHED_CONF}
 
 # Shutdown VILLASnode
 kill $!
 
-conf2json < ${LOCAL_CONF} | jq -S . > ${LOCAL_JSON_CONF}
-jq -S .response < ${FETCHED_CONF} > ${FETCHED_JSON_CONF}
-
 # Compare local config with the fetched one
-diff -u ${FETCHED_JSON_CONF} ${LOCAL_JSON_CONF}
+diff -u <(jq -S .response < ${FETCHED_CONF}) <(jq -S . < ${LOCAL_CONF})
 RC=$?
 
 rm -f ${FETCHED_CONF} ${FETCHED_JSON_CONF} ${LOCAL_JSON_CONF}
