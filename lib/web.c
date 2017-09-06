ba897f2c47ce2a359cdcc9ed3d972908a5130291
Steffen Vogel <post@steffenvogel.de>
2017-09-06 20:32:18 +0200

    web: proper alignment of log output

--- lib/web.c
+++ lib/web.c
@@ -137,10 +137,10 @@ static void logger(int level, const char *msg) {
 		level = LLL_WARN;
 
 	switch (level) {
-		case LLL_ERR:   log_print(global_log, CLR_RED("Web"), "%.*s", len, msg); break;
-		case LLL_WARN:	log_print(global_log, CLR_YEL("Web"), "%.*s", len, msg); break;
-		case LLL_INFO:	log_print(global_log, CLR_WHT("Web"), "%.*s", len, msg); break;
-		default:        log_print(global_log,         "Web",  "%.*s", len, msg); break;
+		case LLL_ERR:   log_print(global_log, CLR_RED("Web  "), "%.*s", len, msg); break;
+		case LLL_WARN:	log_print(global_log, CLR_YEL("Web  "), "%.*s", len, msg); break;
+		case LLL_INFO:	log_print(global_log, CLR_WHT("Web  "), "%.*s", len, msg); break;
+		default:        log_print(global_log,         "Web  ",  "%.*s", len, msg); break;
 	}
 }
 
