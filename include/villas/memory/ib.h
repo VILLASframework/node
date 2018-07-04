3275a09a4e562a0cd6131c71e9d553a6b5f217a6
Dennis Potter <dennis@dennispotter.eu>
2018-07-04 17:27:15 +0200

    Added missing ib.h file

---
+++ include/villas/memory/ib.h
@@ -0,0 +1,31 @@
+/** Memory allocators.
+ *
+ * @file
+ * @author Dennis Potter <dennis@dennispotter.eu>
+ * @copyright 2018, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU General Public License (version 3)
+ *
+ * VILLASnode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************************/
+
+#include <villas/node.h>
+
+struct memory_ib {
+    struct ibv_pd *pd;
+    struct memory_type *parent;
+};
+
+struct ibv_mr* memory_ib_get_mr(struct sample *smps);
