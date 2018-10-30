d2a7107aaafd444817178a198fe2f71dadb87d93
Steffen Vogel <post@steffenvogel.de>
2018-10-30 10:55:02 +0100

    raw: fix raw format without fake headers

--- include/villas/formats/raw.h
+++ include/villas/formats/raw.h
@@ -39,7 +39,7 @@ struct sample;
 
 enum raw_flags {
 	/** Treat the first three values as: sequenceno, seconds, nanoseconds */
-	RAW_FAKE_HEADER	= (1 << 16) | SAMPLE_HAS_TS_ORIGIN | SAMPLE_HAS_SEQUENCE,
+	RAW_FAKE_HEADER	= (1 << 16),
 	RAW_BIG_ENDIAN	= (1 << 7),	/**< Encode data in big-endian byte order */
 
 	RAW_BITS_8	= (3 << 24),	/**< Pack each value as a byte. */
