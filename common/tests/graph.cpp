dc6cfabbac3b2ae57aa719685bffb5da4849e290
Steffen Vogel <post@steffenvogel.de>
2019-01-13 00:42:26 +0100

    update copyright year to 2019

--- common/tests/graph.cpp
+++ common/tests/graph.cpp
@@ -1,7 +1,7 @@
 /** Graph unit test.
  *
  * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
- * @copyright 2017-2018, Steffen Vogel
+ * @copyright 2014-2019, Steffen Vogel
  * @license GNU General Public License (version 3)
  *
  * VILLAScommon
