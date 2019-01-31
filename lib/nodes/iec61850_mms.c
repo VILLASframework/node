9670e325a499c3634a41a5b5e0698269ea92e3c6
Steffen Vogel <post@steffenvogel.de>
2019-01-31 11:59:06 +0100

    codestyle: convert spaces to tabs

--- lib/nodes/iec61850_mms.c
+++ lib/nodes/iec61850_mms.c
@@ -27,259 +27,259 @@
 
 int iec61850_mms_parse_ids(json_t * mms_ids, struct list * domainIDs, struct list * itemIDs)
 {
-    int ret = 0, totalsize = 0;
-    const char * domainID;
-    const char * itemID;
+	int ret = 0, totalsize = 0;
+	const char * domainID;
+	const char * itemID;
 
-    ret = list_init(domainIDs);
-    if (ret)
-        return ret;
+	ret = list_init(domainIDs);
+	if (ret)
+		return ret;
 
-    ret = list_init(itemIDs);
-    if (ret)
-        return ret;
+	ret = list_init(itemIDs);
+	if (ret)
+		return ret;
 
-    size_t i = 0;
-    json_t * mms_id;
-    json_error_t err;
-    json_array_foreach(mms_ids, i, mms_id) {
+	size_t i = 0;
+	json_t * mms_id;
+	json_error_t err;
+	json_array_foreach(mms_ids, i, mms_id) {
 
-        ret = json_unpack_ex(mms_id, &err, 0, "{s: s, s: s}",
-                "domain_ID", &domainID,
-                "item_ID", &itemID
-                );
+		ret = json_unpack_ex(mms_id, &err, 0, "{s: s, s: s}",
+					"domain_ID", &domainID,
+					"item_ID", &itemID
+					);
 
-        if (ret) 
-            warn("Failed to parse configuration while reading domain and item ids");
+		if (ret)
+				warn("Failed to parse configuration while reading domain and item ids");
 
 
-        list_push(domainIDs, (void *) domainID);
-        list_push(itemIDs, (void *) itemID);
+		list_push(domainIDs, (void *) domainID);
+		list_push(itemIDs, (void *) itemID);
 
-        totalsize++;
-    }
-    return totalsize;
+		totalsize++;
+	}
+	return totalsize;
 }
 
 int iec61850_mms_parse(struct node * n, json_t * cfg)
 {
-    int ret;
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	int ret;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
 
-    json_error_t err;
+	json_error_t err;
 
-    const char * host = NULL;
-    json_t * json_in = NULL;
-    json_t * json_out = NULL;
-    json_t * json_signals = NULL;
-    json_t * json_mms_ids = NULL;
+	const char * host = NULL;
+	json_t * json_in = NULL;
+	json_t * json_out = NULL;
+	json_t * json_signals = NULL;
+	json_t * json_mms_ids = NULL;
 
-    ret = json_unpack_ex(cfg, &err, 0, "{ s: s, s: i, s: i, s?: o, s?: o }",
-            "host", &host,
-            "port", &mms->port,
-            "rate", &mms->rate,
-            "in", &json_in,
-            "out", &json_out
-            );
-    mms->host = strdup(host);
+	ret = json_unpack_ex(cfg, &err, 0, "{ s: s, s: i, s: i, s?: o, s?: o }",
+				"host", &host,
+				"port", &mms->port,
+				"rate", &mms->rate,
+				"in", &json_in,
+				"out", &json_out
+				);
+	mms->host = strdup(host);
 
-    if (ret)
-        jerror (&err, "Failed to parse configuration of node %s", node_name(n));
+	if (ret)
+		jerror (&err, "Failed to parse configuration of node %s", node_name(n));
 
-    if (json_in) {
+	if (json_in) {
 
-        ret = json_unpack_ex(json_in, &err, 0, "{ s: o, s: o }",
-                "iec_types", &json_signals,
-                "mms_ids", &json_mms_ids
-                );
-        if (ret)
-            jerror(&err, "Failed to parse configuration of node %s", node_name(n));
+		ret = json_unpack_ex(json_in, &err, 0, "{ s: o, s: o }",
+					"iec_types", &json_signals,
+					"mms_ids", &json_mms_ids
+					);
+		if (ret)
+				jerror(&err, "Failed to parse configuration of node %s", node_name(n));
 
-        ret = iec61850_parse_signals(json_signals, &mms->in.iecTypeList, &n->signals);
-        if (ret <= 0)
-            error("Failed to parse setting 'iecList' of node %s", node_name(n));
+		ret = iec61850_parse_signals(json_signals, &mms->in.iecTypeList, &n->signals);
+		if (ret <= 0)
+				error("Failed to parse setting 'iecList' of node %s", node_name(n));
 
-        mms->in.totalsize = ret;
+		mms->in.totalsize = ret;
 
-        int length_mms_ids = iec61850_mms_parse_ids(json_mms_ids, &mms->in.domain_ids, &mms->in.item_ids);
+		int length_mms_ids = iec61850_mms_parse_ids(json_mms_ids, &mms->in.domain_ids, &mms->in.item_ids);
 
-        int length_iec_types = mms->in.iecTypeList.length;
-        if (length_mms_ids == -1)
-            error("Configuration error in node '%s': json error while parsing", node_name(n));
-        else if (length_iec_types != length_mms_ids)  // length of the lists is not the same
-            error("Configuration error in node '%s': one set of 'mms_ids'(%d value(s)) should match one value of 'iecTypes'(%d value(s))", node_name(n), length_mms_ids, length_iec_types);
-    }
+		int length_iec_types = mms->in.iecTypeList.length;
+		if (length_mms_ids == -1)
+				error("Configuration error in node '%s': json error while parsing", node_name(n));
+		else if (length_iec_types != length_mms_ids)  // length of the lists is not the same
+				error("Configuration error in node '%s': one set of 'mms_ids'(%d value(s)) should match one value of 'iecTypes'(%d value(s))", node_name(n), length_mms_ids, length_iec_types);
+	}
 
-    if (json_out) {
-        ret = json_unpack_ex(json_out, &err, 0, "{ s?: b, s?: i, s: o, s: o }",
-                "test", &mms->out.isTest,
-                "testvalue", &mms->out.testvalue,
-                "iec_types", &json_signals,
-                "mms_ids", &json_mms_ids
-                );
-        if (ret)
-            jerror(&err, "Failed to parse configuration of node %s", node_name(n));
+	if (json_out) {
+		ret = json_unpack_ex(json_out, &err, 0, "{ s?: b, s?: i, s: o, s: o }",
+					"test", &mms->out.isTest,
+					"testvalue", &mms->out.testvalue,
+					"iec_types", &json_signals,
+					"mms_ids", &json_mms_ids
+					);
+		if (ret)
+				jerror(&err, "Failed to parse configuration of node %s", node_name(n));
 
-        ret = iec61850_parse_signals(json_signals, &mms->out.iecTypeList, &n->signals);
-        if (ret <= 0)
-            error("Failed to parse setting 'iecList' of node %s", node_name(n));
+		ret = iec61850_parse_signals(json_signals, &mms->out.iecTypeList, &n->signals);
+		if (ret <= 0)
+				error("Failed to parse setting 'iecList' of node %s", node_name(n));
 
-        mms->out.totalsize = ret;
+		mms->out.totalsize = ret;
 
-        int length_mms_ids = iec61850_mms_parse_ids(json_mms_ids, &mms->out.domain_ids, &mms->out.item_ids);
+		int length_mms_ids = iec61850_mms_parse_ids(json_mms_ids, &mms->out.domain_ids, &mms->out.item_ids);
 
-        int length_iec_types = mms->out.iecTypeList.length;
-        if (length_mms_ids == -1)
-            error("Configuration error in node '%s': json error while parsing", node_name(n));
-        else if (length_iec_types != length_mms_ids)  // length of the lists is not the same
-            error("Configuration error in node '%s': one set of 'mms_ids'(%d value(s)) should match one value of 'iecTypes'(%d value(s))", node_name(n), length_mms_ids, length_iec_types);
-    }
+		int length_iec_types = mms->out.iecTypeList.length;
+		if (length_mms_ids == -1)
+				error("Configuration error in node '%s': json error while parsing", node_name(n));
+		else if (length_iec_types != length_mms_ids)  // length of the lists is not the same
+				error("Configuration error in node '%s': one set of 'mms_ids'(%d value(s)) should match one value of 'iecTypes'(%d value(s))", node_name(n), length_mms_ids, length_iec_types);
+	}
 
-    return 0;
+	return 0;
 }
 
 char * iec61850_mms_print(struct node * n)
 {
-    char * buf;
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	char * buf;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
 
-    buf = strf("host = %s, port = %d, domainID = %s, itemID = %s", mms->host, mms->port, mms->domainID, mms->itemID);
+	buf = strf("host = %s, port = %d, domainID = %s, itemID = %s", mms->host, mms->port, mms->domainID, mms->itemID);
 
-    return buf;
+	return buf;
 }
 
 // create connection to MMS server
 int iec61850_mms_start(struct node * n)
 {
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
 
-    mms->conn = MmsConnection_create();
+	mms->conn = MmsConnection_create();
 
-    /* Connect to MMS Server */
-    MmsError err;
-    if (!MmsConnection_connect(mms->conn, &err, mms->host, mms->port))
-    {
-        error("Cannot connect to MMS server: %s:%d", mms->host, mms->port);
-    }
+	/* Connect to MMS Server */
+	MmsError err;
+	if (!MmsConnection_connect(mms->conn, &err, mms->host, mms->port))
+	{
+		error("Cannot connect to MMS server: %s:%d", mms->host, mms->port);
+	}
 
-    // setup task
-    int ret = task_init(&mms->task, mms->rate, CLOCK_MONOTONIC);
-    if (ret)
-        return ret;
+	// setup task
+	int ret = task_init(&mms->task, mms->rate, CLOCK_MONOTONIC);
+	if (ret)
+		return ret;
 
-    return 0;
+	return 0;
 }
 
 // destroy connection to MMS server
 int iec61850_mms_stop(struct node * n)
 {
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
-    MmsConnection_destroy(mms->conn); // doesn't have return value
-    return 0;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	MmsConnection_destroy(mms->conn); // doesn't have return value
+	return 0;
 }
 
 // read from MMS Server
 int iec61850_mms_read(struct node * n, struct sample * smps[], unsigned cnt, unsigned * release)
 {
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
-    struct sample * smp = smps[0];
-    //struct timespec time;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	struct sample * smp = smps[0];
+	//struct timespec time;
 
-    // read value from MMS server
-    MmsError error;
-    MmsValue * mms_val;
+	// read value from MMS server
+	MmsError error;
+	MmsValue * mms_val;
 
-    task_wait(&mms->task);
+	task_wait(&mms->task);
 
-    // TODO: timestamp von MMS server?
-    //time = time_now();
+	// TODO: timestamp von MMS server?
+	//time = time_now();
 
 
-    smp->flags = SAMPLE_HAS_DATA | SAMPLE_HAS_SEQUENCE;
-    smp->length = 0;
+	smp->flags = SAMPLE_HAS_DATA | SAMPLE_HAS_SEQUENCE;
+	smp->length = 0;
 
-    const char * domainID;
-    const char * itemID;
+	const char * domainID;
+	const char * itemID;
 
-    for (size_t j = 0; j < list_length(&mms->in.iecTypeList); j++)
-    {
-        // get MMS Value from server
-        domainID = (const char *) list_at(&mms->in.domain_ids, j);
-        itemID = (const char *) list_at(&mms->in.item_ids, j);
+	for (size_t j = 0; j < list_length(&mms->in.iecTypeList); j++)
+	{
+		// get MMS Value from server
+		domainID = (const char *) list_at(&mms->in.domain_ids, j);
+		itemID = (const char *) list_at(&mms->in.item_ids, j);
 
-        mms_val = MmsConnection_readVariable(mms->conn, &error, domainID, itemID);
+		mms_val = MmsConnection_readVariable(mms->conn, &error, domainID, itemID);
 
-        if (mms_val == NULL) {
-            warn("Reading MMS value from server failed");
-        }
+		if (mms_val == NULL) {
+				warn("Reading MMS value from server failed");
+		}
 
-        // convert result according data type
-        struct iec61850_type_descriptor * td = (struct iec61850_type_descriptor *) list_at(&mms->in.iecTypeList, j);
+		// convert result according data type
+		struct iec61850_type_descriptor * td = (struct iec61850_type_descriptor *) list_at(&mms->in.iecTypeList, j);
 
-        switch (td->type) {
-            case IEC61850_TYPE_INT32:   smp->data[j].i = MmsValue_toInt32(mms_val); break;
-            case IEC61850_TYPE_INT32U:  smp->data[j].i = MmsValue_toUint32(mms_val); break;
-            case IEC61850_TYPE_FLOAT32: smp->data[j].f = MmsValue_toFloat(mms_val); break;
-            case IEC61850_TYPE_FLOAT64: smp->data[j].f = MmsValue_toDouble(mms_val); break;
-            default: { }
-        }
+		switch (td->type) {
+				case IEC61850_TYPE_INT32:	smp->data[j].i = MmsValue_toInt32(mms_val); break;
+				case IEC61850_TYPE_INT32U:  smp->data[j].i = MmsValue_toUint32(mms_val); break;
+				case IEC61850_TYPE_FLOAT32: smp->data[j].f = MmsValue_toFloat(mms_val); break;
+				case IEC61850_TYPE_FLOAT64: smp->data[j].f = MmsValue_toDouble(mms_val); break;
+				default: { }
+		}
 
-        smp->length++;
-    }
+		smp->length++;
+	}
 
-    MmsValue_delete(mms_val);
+	MmsValue_delete(mms_val);
 
-    return 1;
+	return 1;
 }
 
 // TODO
 int iec61850_mms_write(struct node * n, struct sample * smps[], unsigned cnt, unsigned * release)
 {
-//    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+//	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
 
 
-    return 0;
+	return 0;
 }
 
 int iec61850_mms_destroy(struct node * n)
 {
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
 
-    MmsConnection_destroy(mms->conn);
+	MmsConnection_destroy(mms->conn);
 
-    free(mms->host);
-    free(mms->domainID);
-    free(mms->itemID);
+	free(mms->host);
+	free(mms->domainID);
+	free(mms->itemID);
 
-    return 0;
+	return 0;
 }
 
 int iec61850_mms_fd(struct node * n)
 {
-    struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
+	struct iec61850_mms * mms = (struct iec61850_mms *) n->_vd;
 
-    return task_fd(&mms->task);
+	return task_fd(&mms->task);
 }
 
 
 static struct plugin p = {
-    .name           = "iec61850-8-1",
-    .description    = "IEC 61850-8-1 MMS (libiec61850)",
-    .type           = PLUGIN_TYPE_NODE,
-    .node           = {
-        .vectorize      = 0, /* unlimited */
-        .size           = sizeof(struct iec61850_mms),
-        .type.start     = iec61850_type_start,   // TODO
-        .type.stop      = iec61850_type_stop,    // TODO
-        .parse          = iec61850_mms_parse,       /* done */
-        .print          = iec61850_mms_print,       /* done */
-        .start          = iec61850_mms_start,       /* done */
-        .stop           = iec61850_mms_stop,        // TODO
-        .destroy        = iec61850_mms_destroy,     /* done */
-        .read           = iec61850_mms_read,        /* done */
-        .write          = iec61850_mms_write,       // TODO: Rumpf erstellt
-        .fd             = iec61850_mms_fd
-    }
+	.name		= "iec61850-8-1",
+	.description	= "IEC 61850-8-1 MMS (libiec61850)",
+	.type		= PLUGIN_TYPE_NODE,
+	.node		= {
+		.vectorize	= 0, /* unlimited */
+		.size		= sizeof(struct iec61850_mms),
+		.type.start	= iec61850_type_start,	// TODO
+		.type.stop	= iec61850_type_stop,	// TODO
+		.parse		= iec61850_mms_parse,	/* done */
+		.print		= iec61850_mms_print,	/* done */
+		.start		= iec61850_mms_start,	/* done */
+		.stop		= iec61850_mms_stop,	// TODO
+		.destroy	= iec61850_mms_destroy,	/* done */
+		.read		= iec61850_mms_read,	/* done */
+		.write		= iec61850_mms_write,	// TODO: Rumpf erstellt
+		.fd		= iec61850_mms_fd
+	}
 };
 
 REGISTER_PLUGIN(&p);
