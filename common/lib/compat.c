fb68c375e6587456339e7c27df0cc8a42f1fd112
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2019-02-09 20:09:20 +0000

    added compatability for libjansson 2.9

--- common/lib/compat.c
+++ common/lib/compat.c
@@ -22,6 +22,7 @@
 
 #include <string.h>
 #include <jansson.h>
+#include <unistd.h>
 
 #include <villas/compat.h>
 
@@ -42,5 +43,35 @@ size_t json_dumpb(const json_t *json, char *buffer, size_t size, size_t flags)
 	free(str);
 
 	return len;
+}
+
+static size_t json_loadfd_callback(void *buffer, size_t buflen, void *data)
+{
+	int *fd = (int *) data;
+
+	return (size_t) read(*fd, buffer, buflen);
+}
+
+json_t *json_loadfd(int input, size_t flags, json_error_t *error)
+{
+	return json_load_callback(json_loadfd_callback, (void *) &input, flags, error);
+}
+
+
+static int json_dumpfd_callback(const char *buffer, size_t size, void *data)
+{
+#ifdef HAVE_UNISTD_H
+	int *dest = (int *)data;
+
+	if (write(*dest, buffer, size) == (ssize_t)size)
+		return 0;
+#endif
+
+	return -1;
+}
+
+int json_dumpfd(const json_t *json, int output, size_t flags)
+{
+    return json_dump_callback(json, json_dumpfd_callback, (void *) &output, flags);
 }
 #endif
