d19ae55c71ecdb3ba6de6a88991b6ceecdc7a5ec
Steffen Vogel <post@steffenvogel.de>
2019-02-18 01:11:16 +0100

    hook: propagate error in hook_process()

--- src/villas-hook.cpp
+++ src/villas-hook.cpp
@@ -215,7 +215,9 @@ check:		if (optarg == endptr)
 
 		unsigned send = recv;
 
-		hook_process(&h, smps, (unsigned *) &send);
+		ret = hook_process(&h, smps, (unsigned *) &send);
+		if (ret < 0)
+			throw RuntimeError("Failed to process samples");
 
 		sent = io_print(&io, smps, send);
 		if (sent < 0)
