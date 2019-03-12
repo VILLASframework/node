3873847de0e97e62933c2c77a3a06bbb41aab373
Marvin Klimke <marvin.klimke@rwth-aachen.de>
2019-03-12 16:46:28 +0100

    rtp: adapt test script to work with remote server

--- tests/integration/pipe-loopback-rtp-remote.sh
+++ tests/integration/pipe-loopback-rtp-remote.sh
@@ -31,10 +31,10 @@ fi
 LOCAL_ADDR=137.226.133.195
 REMOTE_ADDR=157.230.251.200
 REMOTE_USER=root
-
 REMOTE=ssh ${REMOTE_USER}@${REMOTE_ADDR}
 
-export PATH=/projects/villas/node/build/src:${PATH}
+PATH=/projects/villas/node/build/src:${PATH}
+REMOTE PATH=/projects/villas/node/build/src:${PATH}
 
 SCRIPT=$(realpath $0)
 SCRIPTPATH=$(dirname ${SCRIPT})
@@ -72,14 +72,14 @@ cat > ${CONFIG_FILE_SRC} << EOF
 				"b" : 0.5
 			},
 			"in" : {
-				"address" : "0.0.0.0:12002",
+				"address" : "0.0.0.0:33466",
 				"signals" : {
 					"count" : 5,
 					"type" : "float"
 				}
 			},
 			"out" : {
-				"address" : "${REMOTE_ADDR}:12000"
+				"address" : "${REMOTE_ADDR}:33464"
 			}
 		}
 	}
@@ -88,7 +88,7 @@ EOF
 
 # UDP ports: 33434 - 33534
 
-${REMOTE} cat > ${CONFIG_FILE_DEST} << EOF
+cat > ${CONFIG_FILE_DEST} << EOF
 {
 	"logging" : {
 		"level" : "debug"
@@ -109,19 +109,21 @@ ${REMOTE} cat > ${CONFIG_FILE_DEST} << EOF
 				"b" : 0.5
 			},
 			"in" : {
-				"address" : "0.0.0.0:12000",
+				"address" : "0.0.0.0:33464",
 				"signals" : {
 					"count" : 5,
 					"type" : "float"
 				}
 			},
 			"out" : {
-				"address" : "${LOCAL_ADDR}:12002"
+				"address" : "${LOCAL_ADDR}:33466"
 			}
 		}
 	}
 }
 EOF
+
+scp ${CONFIG_FILE_DEST} ${REMOTE_USER}@${REMOTE_ADDR}:${CONFIG_FILE_DEST}
 
 ${REMOTE} villas-pipe -l ${NUM_SAMPLES} ${CONFIG_FILE_DEST} rtp_node > ${OUTPUT_FILE} &
 PID=$!
@@ -129,7 +131,7 @@ PID=$!
 sleep 1
 
 villas-signal mixed -v 5 -r ${RATE} -l ${NUM_SAMPLES} | tee ${INPUT_FILE} | \
-villas-pipe ${CONFIG_FILE_SRC} rtp_node > ${OUTPUT_FILE}
+villas-pipe ${CONFIG_FILE_SRC} rtp_node
 
 scp ${REMOTE_USER}@${REMOTE_ADDR}:${OUTPUT_FILE} ${OUTPUT_FILE}
 
@@ -137,7 +139,7 @@ scp ${REMOTE_USER}@${REMOTE_ADDR}:${OUTPUT_FILE} ${OUTPUT_FILE}
 villas-test-cmp ${CMPFLAGS} ${INPUT_FILE} ${OUTPUT_FILE}
 RC=$?
 
-rm ${INPUT_FILE} ${OUTPUT_FILE} ${CONFIG_FILE_SRC}
+rm ${INPUT_FILE} ${OUTPUT_FILE} ${CONFIG_FILE_DEST} ${CONFIG_FILE_SRC}
 ${REMOTE} rm ${OUTPUT_FILE} ${CONFIG_FILE_DEST}
 
 kill ${PID}
