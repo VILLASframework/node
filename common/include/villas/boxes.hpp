65600fde0b3253d4541dc1ae640e865ffd135950
Steffen Vogel <post@steffenvogel.de>
2019-04-07 15:45:55 +0200

    move box drawing chars to boxes.hpp

--- common/include/villas/boxes.hpp
+++ common/include/villas/boxes.hpp
@@ -23,9 +23,7 @@
 
 #pragma once
 
-/* Alternate character set
- *
- * The suffixed of the BOX_ macro a constructed by
+/* The suffixed of the BOX_ macro a constructed by
  * combining the following letters in the written order:
  *   - U for a line facing upwards
  *   - D for a line facing downwards
@@ -35,13 +33,36 @@
  * E.g. a cross can be constructed by combining all line fragments:
  *    BOX_UDLR
  */
-#define BOX(chr)	"\e(0" chr "\e(B"
-#define BOX_LR		BOX("\x71") /**< Boxdrawing: ─ */
-#define BOX_UD		BOX("\x78") /**< Boxdrawing: │ */
-#define BOX_UDR		BOX("\x74") /**< Boxdrawing: ├ */
-#define BOX_UDLR	BOX("\x6E") /**< Boxdrawing: ┼ */
-#define BOX_UDL		BOX("\x75") /**< Boxdrawing: ┤ */
-#define BOX_ULR		BOX("\x76") /**< Boxdrawing: ┴ */
-#define BOX_UL		BOX("\x6A") /**< Boxdrawing: ┘ */
-#define BOX_DLR		BOX("\x77") /**< Boxdrawing: ┘ */
-#define BOX_DL		BOX("\x6B") /**< Boxdrawing: ┘ */
+#if 0 /* Alternate character set */
+  #define BOX(chr)	"\e(0" chr "\e(B"
+  #define BOX_LR	BOX("\x71") /**< Boxdrawing: ─ */
+  #define BOX_UD	BOX("\x78") /**< Boxdrawing: │ */
+  #define BOX_UDR	BOX("\x74") /**< Boxdrawing: ├ */
+  #define BOX_UDLR	BOX("\x6E") /**< Boxdrawing: ┼ */
+  #define BOX_UDL	BOX("\x75") /**< Boxdrawing: ┤ */
+  #define BOX_ULR	BOX("\x76") /**< Boxdrawing: ┴ */
+  #define BOX_UL	BOX("\x6A") /**< Boxdrawing: ┘ */
+  #define BOX_DLR	BOX("\x77") /**< Boxdrawing: ┘ */
+  #define BOX_DL	BOX("\x6B") /**< Boxdrawing: ┘ */
+#elif 1 /* UTF-8 */
+  #define BOX_LR	"─" /**< Boxdrawing: ─ */
+  #define BOX_UD	"│" /**< Boxdrawing: │ */
+  #define BOX_UDR	"├" /**< Boxdrawing: ├ */
+  #define BOX_UDLR	"┼" /**< Boxdrawing: ┼ */
+  #define BOX_UDL	"┤" /**< Boxdrawing: ┤ */
+  #define BOX_ULR	"┴" /**< Boxdrawing: ┴ */
+  #define BOX_UL	"┘" /**< Boxdrawing: ┘ */
+  #define BOX_DLR	"┬" /**< Boxdrawing: ┘ */
+  #define BOX_DL	"┐" /**< Boxdrawing: ┘ */
+  #define BOX_UR	"└" /**< Boxdrawing: └ */
+#else /* ASCII */
+  #define BOX_LR	"-" /**< Boxdrawing: ─ */
+  #define BOX_UD	"|" /**< Boxdrawing: │ */
+  #define BOX_UDR	"+" /**< Boxdrawing: ├ */
+  #define BOX_UDLR	"+" /**< Boxdrawing: ┼ */
+  #define BOX_UDL	"+" /**< Boxdrawing: ┤ */
+  #define BOX_ULR	"+" /**< Boxdrawing: ┴ */
+  #define BOX_UL	"+" /**< Boxdrawing: ┘ */
+  #define BOX_DLR	"+" /**< Boxdrawing: ┘ */
+  #define BOX_DL	"+" /**< Boxdrawing: ┘ */
+#endif
