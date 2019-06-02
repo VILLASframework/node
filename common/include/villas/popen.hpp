f4dcbf4363c095f4ea9c147669c9d724a58a936b
Steffen Vogel <post@steffenvogel.de>
2019-06-02 20:48:04 +0200

    popen: added iostream-based bidirectional popen()

---
+++ common/include/villas/popen.hpp
@@ -0,0 +1,84 @@
+/** Bi-directional popen
+ *
+ * @file
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @copyright 2014-2019, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU General Public License (version 3)
+ *
+ * VILLAScommon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************************/
+
+#pragma once
+
+#include <signal.h>
+
+#include <ext/stdio_filebuf.h>
+
+#include <string>
+#include <istream>
+#include <ostream>
+#include <memory>
+
+namespace villas {
+namespace utils {
+
+class Popen {
+
+public:
+	using char_type = char;
+	using stdio_buf = __gnu_cxx::stdio_filebuf<char_type>;
+
+	Popen(const std::string &command);
+	~Popen();
+
+	void open();
+	int close();
+	void kill(int signal = SIGINT);
+
+	std::istream &in()
+	{
+		return *(input.stream);
+	}
+
+	std::ostream &out()
+	{
+		return *(output.stream);
+	}
+
+protected:
+	std::string command;
+	pid_t pid;
+
+	struct {
+		std::unique_ptr<std::istream> stream;
+		std::unique_ptr<stdio_buf> buffer;
+	} input;
+
+	struct {
+		std::unique_ptr<std::ostream> stream;
+		std::unique_ptr<stdio_buf> buffer;
+	} output;
+
+};
+
+} /* namespace utils */
+} /* namespace villas */
+
+template<typename T>
+std::istream& operator>>(villas::utils::Popen& po, T& value)
+{
+	return *(po.input.stream) >> value;
+}
