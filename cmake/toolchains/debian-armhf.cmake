a9f85715774f795065d4964942ae32c8ed23bb9a
Steffen Vogel <post@steffenvogel.de>
2019-06-26 23:33:20 +0200

    docker: add Dockerfile for armhf cross-compilation based on Debian Buster / Multiarch

---
+++ cmake/toolchains/debian-armhf.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR armv7l)
+
+set(TRIPLET "arm-linux-gnueabihf")
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   "/usr/bin/${TRIPLET}-gcc")
+SET(CMAKE_CXX_COMPILER "/usr/bin/${TRIPLET}-g++")
+
+set(CMAKE_C_FLAGS "-mfloat-abi=hard ${CMAKE_C_FLAGS}" CACHE STRING "Buildroot CFLAGS")
+set(CMAKE_CXX_FLAGS "-mfloat-abi=hard ${CMAKE_CXX_FLAGS}" CACHE STRING "Buildroot CXXFLAGS")
+set(CMAKE_EXE_LINKER_FLAGS " ${CMAKE_EXE_LINKER_FLAGS}" CACHE STRING "Buildroot LDFLAGS")
+
+set(CMAKE_LIBRARY_PATH "/usr/lib/${TRIPLET}")
+
