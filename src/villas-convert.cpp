1f4a7a540e3e364b917316efde9a445a8cc994e0
Steffen Vogel <post@steffenvogel.de>
2020-01-09 09:38:20 +0100

    convert: add missing name to IO dir

--- src/villas-convert.cpp
+++ src/villas-convert.cpp
@@ -54,6 +54,7 @@ public:
 			throw RuntimeError("Failed to initialize memory");
 
 		for (unsigned i = 0; i < ARRAY_LEN(dirs); i++) {
+			dirs[i].name = i == 0 ? "in" : "out";
 			dirs[i].format = "villas.human";
 			dirs[i].io.state = State::DESTROYED;
 		}
