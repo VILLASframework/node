86f8997b05159927a031c4ebf4255b05f8c908b0
Steffen Vogel <post@steffenvogel.de>
2020-06-11 15:58:02 +0200

    gpio: add new IP for AXI programmable GPIO

---
+++ fpga/lib/ips/gpio.cpp
@@ -0,0 +1,52 @@
+/** AXI General Purpose IO (GPIO)
+ *
+ * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
+ * @copyright 2017-2020, Steffen Vogel
+ * @license GNU General Public License (version 3)
+ *
+ * VILLASfpga
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************************/
+
+#include <unistd.h>
+#include <errno.h>
+
+#include <villas/config.h>
+#include <villas/plugin.hpp>
+
+#include <villas/kernel/kernel.hpp>
+
+#include <villas/fpga/card.hpp>
+#include <villas/fpga/ips/gpio.hpp>
+
+namespace villas {
+namespace fpga {
+namespace ip {
+
+
+// instantiate factory to make available to plugin infrastructure
+static GeneralPurposeIOFactory factory;
+
+bool
+GeneralPurposeIO::init()
+{
+	//const uintptr_t base = getBaseAddr(registerMemory);
+
+	return true;
+}
+
+} // namespace ip
+} // namespace fpga
+} // namespace villas
