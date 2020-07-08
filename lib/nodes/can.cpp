5df815765aeeba739a676ff5a3e36f9a65c46eb2
Steffen Vogel <post@steffenvogel.de>
2020-07-08 13:32:33 +0200

    can: fix authors

--- lib/nodes/can.cpp
+++ lib/nodes/can.cpp
@@ -1,7 +1,6 @@
 /** Node-type: CAN bus
  *
  * @author Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
- * @author Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
  * @copyright 2014-2020, Institute for Automation of Complex Power Systems, EONERC
  * @license GNU General Public License (version 3)
  *
