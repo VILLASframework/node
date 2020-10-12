3ea592cf71753947232880eff34ab3190f447545
Manuel Pitz <manuel.pitz@eonerc.rwth-aachen.de>
2020-10-12 15:26:03 +0200

    Move villasDump from dft to dumper class

---
+++ include/villas/dumper.hpp
@@ -0,0 +1,42 @@
+/** Dump fields and values in a socket to plot them with villasDump.py.
+ *
+ * @file
+ * @author Manuel Pitz <manuel.pitz@eonerc.rwth-aachen.de>
+ * @copyright 2014-2020, Institute for Automation of Complex Power Systems, EONERC
+ * @license GNU General Public License (version 3)
+ *
+ * VILLASnode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *********************************************************************************/
+
+#include <string>
+
+namespace villas {
+namespace node {
+
+class Dumper {
+	protected:
+		int socketFd;
+		std::string socketName;
+
+	public:
+		Dumper(std::string socketNameIn);
+		~Dumper();
+		int openSocket(std::string socketNameIn);
+		void closeSocket();
+		void writeData(uint len, double* yData, double* xData = nullptr);
+    };
+}
+}
