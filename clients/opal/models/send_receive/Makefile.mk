5a95fc0bd69b3786d82e505ee499d8539842184a
Steffen Vogel <stvogel@eonerc.rwth-aachen.de>
2021-06-10 09:35:48 +0000

    fix compilation of OPAL AsyncIP client code on new opalrtlinux3 64-bit targets
    Thanks to Cory Paquet-Clouston (OPAL-RT) for the fix

--- clients/opal/models/send_receive/Makefile.mk
+++ clients/opal/models/send_receive/Makefile.mk
@@ -56,8 +56,8 @@ ifneq ($(RTLAB_ROOT),)
 	INCLUDES += -I$(RTLAB_ROOT)/common/include_target
 endif
 
-CC_OPTS  = -m32 -std=c99 -D_GNU_SOURCE -MMD
-LD_OPTS  = -m32
+CC_OPTS  = -std=c99 -D_GNU_SOURCE -MMD
+LD_OPTS  = 
 OBJS     = main.o msg.o utils.o socket.o $(INTEL_OBJS)
 
 ifneq ($(PROTOCOL),)
