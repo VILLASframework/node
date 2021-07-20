b77403d907f594e969a8417bba5b17a3ad3d0201
Steffen Vogel <post@steffenvogel.de>
2021-07-20 12:23:50 +0200

    json.edgeflex: improve error handling

--- lib/formats/json_edgeflex.cpp
+++ lib/formats/json_edgeflex.cpp
@@ -31,23 +31,32 @@ int JsonEdgeflexFormat::packSample(json_t **json_smp, const struct sample *smp)
 {
 	json_error_t err;
 	json_t *json_value;
-	json_t *json_created = nullptr;
 
-	if (smp->flags & (int) SampleFlags::HAS_TS_ORIGIN)
-		json_created = json_integer(time_to_double(&smp->ts.origin) * 1e3);
-
+	if (smp->length < 1)
+		return -1;
 
 	struct signal *sig = (struct signal *) vlist_at_safe(smp->signals, 0);
 	if (!sig)
 		return -1;
 
+	if (sig->type != SignalType::FLOAT)
+		return -1;
 
 	json_value = json_pack_ex(&err, 0, "{ s: f }",
 		"value", smp->data[0].f
 	);
+	if (json_value == nullptr)
+		return -1;
 
-	if (json_created)
+	if (smp->flags & (int) SampleFlags::HAS_TS_ORIGIN) {
+		json_t *json_created = json_integer(time_to_double(&smp->ts.origin) * 1e3);
 		json_object_set_new(json_value, "created", json_created);
+	}
+
+	*json_smp = json_value;
+
+	return 0;
+}
 
 int JsonEdgeflexFormat::unpackSample(json_t *json_smp, struct sample *smp)
 {
