53e62a6cfcca694af4752c01d93553645b67be4e
Steffen Vogel <post@steffenvogel.de>
2022-05-19 17:40:10 +0200

    relicense under Apache 2.0

--- common/lib/memory_manager.cpp
+++ common/lib/memory_manager.cpp
@@ -2,22 +2,7 @@
  *
  * @author Daniel Krebs <github@daniel-krebs.net>
  * @copyright 2014-2022, Institute for Automation of Complex Power Systems, EONERC
- * @license GNU General Public License (version 3)
- *
- * VILLAScommon
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * @license Apache License 2.0
  *********************************************************************************/
 
 #include <memory>
