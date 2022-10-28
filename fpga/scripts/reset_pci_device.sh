ab994f2c87301b6e12709592d1bbbc5f114bb76a
Steffen Vogel <post@steffenvogel.de>
2022-10-28 08:07:13 -0400

    fix script to reset PCIe card

--- fpga/scripts/reset_pci_device.sh
+++ fpga/scripts/reset_pci_device.sh
@@ -23,13 +23,12 @@
 ##################################################################################
 
 if [ "$#" -ne 1 ]; then
-        echo "usage: $0 BUS:DEV:FNC"
+        echo "usage: $0 BUS:DEV.FNC"
         exit 1
 fi
 
 BDF=$1
 
-echo "1" > /sys/bus/pci/devices/$BDF/remove
+echo "1" > /sys/bus/pci/devices/${BDF}/remove
 echo "1" > /sys/bus/pci/rescan
-echo "1" > /sys/bus/pci/devices/$BDF/enable
-
+echo "1" > /sys/bus/pci/devices/${BDF}/reset
