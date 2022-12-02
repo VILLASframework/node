b135a4c027f911c3dc4069bad95b99bd4d6fac60
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2022-12-02 11:52:12 +0100

    refactor vfio code. split into 3 files and remove friedship between classes
    Signed-off-by: Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>

---
+++ common/include/villas/kernel/vfio_container.hpp
@@ -0,0 +1,82 @@
+/** Virtual Function IO wrapper around kernel API
+ *
+ * @file
+ * @author Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
+ * @author Steffen Vogel <svogel2@eonerc.rwth-aachen.de>
+ * @author Daniel Krebs <github@daniel-krebs.net>
+ * @copyright 2022, Niklas Eiling
+ * @copyright 2014-2021, Steffen Vogel
+ * @copyright 2018, Daniel Krebs
+ *********************************************************************************/
+
+#pragma once
+
+#include <list>
+#include <vector>
+#include <memory>
+
+#include <linux/vfio.h>
+#include <sys/mman.h>
+
+#include "vfio_group.hpp"
+
+namespace villas {
+namespace kernel {
+namespace vfio {
+
+class Container {
+private:
+	//This is a singleton: There can only be one container to rule them all.
+	Container();
+public:
+	//The Container instance is lazily initialized and correctly destroyed.
+	static Container* getInstance()
+	{
+		static Container instance;
+		return &instance;
+	};
+	//No copying allowed
+	Container(Container const&) = delete;
+	void operator=(Container const&) = delete;
+
+	~Container();
+
+	void dump();
+
+	void attachGroup(std::shared_ptr<Group> group);
+
+	std::shared_ptr<Device> attachDevice(const std::string& name, int groupIndex);
+	std::shared_ptr<Device> attachDevice(const pci::Device &pdev);
+
+	/**
+	 * @brief Map VM to an IOVA, which is accessible by devices in the container
+	 * @param virt		virtual address of memory
+	 * @param phys		IOVA where to map @p virt, -1 to use VFIO internal allocator
+	 * @param length	size of memory region in bytes
+	 * @return		IOVA address, UINTPTR_MAX on failure
+	 */
+	uintptr_t memoryMap(uintptr_t virt, uintptr_t phys, size_t length);
+
+	/** munmap() a region which has been mapped by vfio_map_region() */
+	bool memoryUnmap(uintptr_t phys, size_t length);
+
+	bool isIommuEnabled() const
+	{ return this->hasIommu; }
+private:
+	std::shared_ptr<Group> getOrAttachGroup(int index);
+
+	int fd;
+	int version;
+	int extensions;
+	uint64_t iova_next;			/**< Next free IOVA address */
+	bool hasIommu;
+
+	/// All groups bound to this container
+	std::list<std::shared_ptr<Group>> groups;
+
+	Logger log;
+};
+
+} /* namespace vfio */
+} /* namespace kernel */
+} /* namespace villas */
---
+++ common/include/villas/kernel/vfio_device.hpp
@@ -0,0 +1,98 @@
+/** Virtual Function IO wrapper around kernel API
+ *
+ * @file
+ * @author Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
+ * @author Steffen Vogel <svogel2@eonerc.rwth-aachen.de>
+ * @author Daniel Krebs <github@daniel-krebs.net>
+ * @copyright 2022, Niklas Eiling
+ * @copyright 2014-2021, Steffen Vogel
+ * @copyright 2018, Daniel Krebs
+ *********************************************************************************/
+
+#pragma once
+
+#include <list>
+#include <vector>
+#include <memory>
+#include <string>
+
+#include <linux/vfio.h>
+#include <sys/mman.h>
+
+#include "pci.hpp"
+#include <villas/log.hpp>
+
+#define VFIO_PATH	"/dev/vfio/"
+#define VFIO_DEV	VFIO_PATH "vfio"
+
+#ifndef VFIO_NOIOMMU_IOMMU
+  #define VFIO_NOIOMMU_IOMMU 8
+#endif
+
+namespace villas {
+namespace kernel {
+namespace vfio {
+
+class Device {
+public:
+	Device(const std::string &name, int groupFileDescriptor, const kernel::pci::Device *pci_device = nullptr);
+
+	~Device();
+
+	bool reset();
+
+	/** Map a device memory region to the application address space (e.g. PCI BARs) */
+	void* regionMap(size_t index);
+
+	/** munmap() a region which has been mapped by vfio_map_region() */
+	bool regionUnmap(size_t index);
+
+	/** Get the size of a device memory region */
+	size_t regionGetSize(size_t index);
+
+
+	/** Enable memory accesses and bus mastering for PCI device */
+	bool pciEnable();
+
+	int pciMsiInit(int efds[32]);
+	int pciMsiDeinit(int efds[32]);
+	bool pciMsiFind(int nos[32]);
+
+	bool isVfioPciDevice() const;
+	bool pciHotReset();
+
+	int getFileDescriptor() const
+	{ return fd; }
+
+	void dump();
+
+	bool isAttachedToGroup() const
+	{ return attachedToGroup; }
+
+	void setAttachedToGroup()
+	{ this->attachedToGroup = true; }
+
+private:
+	/// Name of the device as listed under
+	/// /sys/kernel/iommu_groups/[vfio_group::index]/devices/
+	std::string name;
+
+	/// VFIO device file descriptor
+	int fd;
+
+	bool attachedToGroup;
+
+	struct vfio_device_info info;
+
+	std::vector<struct vfio_irq_info> irqs;
+	std::vector<struct vfio_region_info> regions;
+	std::vector<void*> mappings;
+
+	/**< libpci handle of the device */
+	const kernel::pci::Device *pci_device;
+
+	Logger log;
+};
+} /* namespace vfio */
+} /* namespace kernel */
+} /* namespace villas */
