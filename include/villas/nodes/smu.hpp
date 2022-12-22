e739ca15b1f3fdfc276efe275fb470ef9efbca45
Manuel Pitz <manuel.pitz@eonerc.rwth-aachen.de>
2022-12-22 20:27:16 +0100

    WIP compiles but cannot load device driver

--- include/villas/nodes/smu.hpp
+++ include/villas/nodes/smu.hpp
@@ -12,13 +12,195 @@
 #include <villas/node.hpp>
 #include <villas/queue_signalled.h>
 
+extern "C" {
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+}
+
+
+#define smu_Nch 8
+
 namespace villas {
 namespace node {
+
+//static const smu_dsp_t dsp_cfg_default = {FR_1PPS, {"1","1","1","1","1","1","1","1"}, {"V1", "V2", "V3", "V4", "I1", "I2", "I3", "I4"}};
+//const smu_dsp_t dsp_cfg_default = {FR_10PPS, {"1","1","1","1","1","1","1","1"}, {"V1", "V2", "V3", "V4", "I1", "I2", "I3", "I4"}, "dsp_raw"};
+//const smu_net_t net_cfg_default = {"id", "", 7080, IPv4, TCP, "JSON", "", 7090, IPv4, TCP, "net_raw", ""};
+
+/// ---------- SMU: DAQ settings section ----------
+
+/**
+ * @brief DAQ sampling rate (kHz / kSPS)
+ *
+ */
+typedef enum : unsigned char
+{
+    FS_1kSPS   =   1,
+    FS_2kSPS   =   2,
+    FS_5kSPS   =   5,
+    FS_10kSPS  =  10,
+    FS_20kSPS  =  20,
+    FS_25kSPS  =  25,
+    FS_50kSPS  =  50,
+    FS_100kSPS = 100,
+    FS_200kSPS = 200
+} daq_rate_t;
+
+/**
+ * @brief DAQ buffer frame rate (Hz / FPS)
+ *
+ */
+typedef enum : unsigned char
+{
+    FB_10FPS  =  10,
+    FB_20FPS  =  20,
+    FB_50FPS  =  50,
+    FB_100FPS = 100,
+    FB_200FPS = 200
+} daq_buff_t;
+
+/**
+ * @brief DAQ acquisition mode
+ *
+ */
+typedef enum : unsigned char
+{
+    MODE_ONESHOT,                   /*!< DAQ runs for 1 second */
+    MODE_FREERUN,                   /*!< DAQ runs continuously */
+    MODE_VIRTUAL,                   /*!< DAQ runs virtually */
+    MODE_MAX
+} daq_mode_t;
+
+/**
+ * @brief DAQ synchronization mode
+ *
+ */
+typedef enum : unsigned char
+{
+    SYNC_NONE,                      /*!< Time base synchronization disabled */
+    SYNC_PPS,                       /*!< Time base synchronization from GPS via pulse-per-second */
+    SYNC_NTP,                       /*!< Time base synchronization from Server via network-time-protocol */
+    SYNC_MAX
+} daq_sync_t;
+
+/**
+ * @brief DAQ parameters
+ *
+ */
+typedef struct
+{
+    daq_rate_t rate;                /*!< DAQ sampling rate */
+    daq_buff_t buff;                /*!< DAQ buffer frame rate */
+    daq_mode_t mode;                /*!< DAQ acquisition mode */
+    daq_sync_t sync;                /*!< DAQ synchronization mode */
+} __attribute__((__packed__)) smu_daq_t;
+
+/// ---------- SMU: DSP settings section ----------
+
+/**
+ * @brief DSP reporting rate (Hz / PPS)
+ *
+ */
+typedef enum : unsigned short int
+{
+    FR_EVENT  = 0,
+    FR_1PPS   = 1,
+    FR_2PPS   = 2,
+    FR_5PPS   = 5,
+    FR_10PPS  = 10,
+    FR_20PPS  = 20,
+    FR_25PPS  = 25,
+    FR_50PPS  = 50,
+    FR_100PPS = 100
+} dsp_rate_t;
+
+/**
+ * @brief DSP parameters
+ *
+ */
+// typedef struct
+// {
+//     dsp_rate_t  rate;               /*!< DSP reporting rate */
+//     QStringList state;              /*!< DSP channels state */
+//     QStringList alias;              /*!< DSP channels alias */
+// } smu_dsp_t;
+
+/**
+ * @brief SMU multi-channel sample code
+ *
+ */
+typedef struct {
+    int16_t ch[smu_Nch];
+} __attribute__((__packed__)) smu_mcsc_t;
+
+/**
+ * @brief SMU multi-channel sample real
+ *
+ */
+typedef struct {
+    float ch[smu_Nch];
+} __attribute__((__packed__)) smu_mcsr_t;
+
+/**
+ * @brief SMU multi-channel conversion coefficients
+ *
+ */
+typedef struct {
+    float k[smu_Nch];
+} __attribute__((__packed__)) smu_mcsk_t;
+
+
+/**
+ * @brief  IOCTL definitions
+ *
+ * Here are defined the different IOCTL commands used
+ * to communicate from the userspace to the kernel module.
+ */
+#define SMU_DEV             "/dev/smu"
+#define SMU_IOC_MAGIC       'k'
+#define SMU_IOC_REG_TASK    _IO (SMU_IOC_MAGIC, 0)
+#define SMU_IOC_RESET       _IO (SMU_IOC_MAGIC, 1)
+#define SMU_IOC_START       _IO (SMU_IOC_MAGIC, 2)
+#define SMU_IOC_STOP        _IO (SMU_IOC_MAGIC, 3)
+#define SMU_IOC_SET_CONF    _IOW(SMU_IOC_MAGIC, 4, smu_conf_t*)
+#define SMU_IOC_GET_CONF    _IOR(SMU_IOC_MAGIC, 5, smu_conf_t*)
+
+/**
+ * @brief  Signal definitions
+ *
+ * Here are defined the different values used to send
+ * signals to the userspace
+ */
+#define SMU_SIG_SYNC        41
+#define SMU_SIG_DATA        42
+
+#define PAGE_NUM    800
+#define PAGE_SIZE   4096
+#define MMAP_SIZE   (PAGE_SIZE*PAGE_NUM)
 
 class SMUNode : public Node {
 
 private:
 	float counter;
+    uint mem_pos;
+    smu_daq_t daq_cfg_default;
+    int fd;
+    uint8_t* shared_mem;
+    uint32_t shared_mem_pos;
+    uint32_t shared_mem_inc;
+    uint32_t shared_mem_dim;
+    struct sigaction act;
 
 protected:
 	virtual
@@ -26,12 +208,17 @@ protected:
 
 public:
 	SMUNode(const std::string &name = "");
+    static void data_available_signal(int, siginfo_t *info, void*);
+    static void sync_signal(int, siginfo_t *info, void*);
 
 	virtual
 	int prepare();
 
 	virtual
 	int start();
+
+	virtual
+	int stop();
 };
 
 } /* namespace node */
--- lib/nodes/smu.cpp
+++ lib/nodes/smu.cpp
@@ -18,7 +18,9 @@ using namespace villas;
 using namespace villas::node;
 using namespace villas::utils;
 
-SMUNode::SMUNode(const std::string &name)
+SMUNode::SMUNode(const std::string &name) :
+	mem_pos(0),
+	daq_cfg_default({FS_10kSPS, FB_10FPS, MODE_FREERUN, SYNC_PPS})
 {
     return;
 }
@@ -36,11 +38,148 @@ int SMUNode::start()
 	       state == State::PAUSED);
 
 	int ret = Node::start();
+
+
+    fd = open(SMU_DEV, O_RDWR);
+    if (fd < 0){
+    //     /*SMU_LOGE(TAG,"[%1] fail to open the device driver");
+    //     smuCFG->error.daq = SMU_ERR_DRV_NOT_FOUND;*/
+    //     return;
+        logger->warn("fail to open the device driver");
+    }
+
+    // Associate kernel-to-user Task ID
+    if (ioctl(fd, SMU_IOC_REG_TASK, 0)){
+        /*SMU_LOGE(TAG,"[%1] fail to register the driver");
+        smuCFG->error.daq = SMU_ERR_DRV_NOT_ASSOC;*/
+        logger->warn("fail to register the driver");
+        close(fd);
+        // return;
+    }
+
+    // Map kernel-to-user shared memory
+    void *sh_mem = mmap(nullptr, MMAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    shared_mem = (uint8_t*)sh_mem;
+    if (shared_mem == MAP_FAILED){
+    //     /*SMU_LOGE(TAG,"[%1] fail to map driver memory");
+    //     smuCFG->error.daq = SMU_ERR_DRV_MMAP_FAIL;*/
+    //     // return;
+        logger->warn("fail to map driver memory");
+
+    }
+
+    // Install PPS signal handler
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = (SA_SIGINFO | SA_RESTART);
+    act.sa_sigaction = sync_signal;
+    if (sigaction(SMU_SIG_SYNC, &act, nullptr)){
+    //     /*SMU_LOGE(TAG,"[%1] fail to install PPS handler");
+    //     smuCFG->error.daq = SMU_ERR_DRV_NO_SIGNAL;*/
+    //     // return;
+        logger->warn("fail to install PPS handler");
+
+    }
+
+    // Install ADC signal handler
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = (SA_SIGINFO | SA_RESTART);
+    act.sa_sigaction = data_available_signal;
+    if (sigaction(SMU_SIG_DATA, &act, nullptr)){
+        /*SMU_LOGE(TAG,"[%1] fail to install ADC handler");
+        smuCFG->error.daq = SMU_ERR_DRV_NO_SIGNAL;*/
+        // return;
+        logger->warn("fail to install ADC handler");
+
+    }
+
+    // Check status
+    /*if (smuCFG->error.daq){
+        SMU_LOGW(TAG,"[%1] cannot configure the module");
+        return;
+    }*/
+	
+    // Update DAQ memory configuration
+    shared_mem_pos = 0;
+    shared_mem_inc = sizeof(smu_mcsc_t);
+    shared_mem_dim = daq_cfg_default.rate;
+
+
+    // Stop DAQ driver
+    if (ioctl(fd, SMU_IOC_STOP, 0)){
+        /*SMU_LOGE(TAG,"[%1] fail to stop the driver");
+        smuCFG->error.daq = SMU_ERR_DRV_NO_STOP;*/
+        logger->warn("fail to stop the driver");
+    }
+
+    // Configure DAQ driver
+    if (::write(fd, &daq_cfg_default, sizeof(daq_cfg_default))){
+        /*smuCFG->error.daq = SMU_ERR_DRV_NOT_CONF;
+        SMU_LOGE(TAG,"[%1] fail to configure the driver");*/
+        // return;
+        logger->warn("fail to configure the driver");
+
+    }
+
+    // Start DAQ driver
+    if (ioctl(fd, SMU_IOC_START, 0)){
+        /*SMU_LOGE(TAG,"[%1] fail to start the driver");
+        smuCFG->error.daq = SMU_ERR_DRV_NO_START;*/
+    //     return;
+        logger->warn("fail to start the driver");
+
+    }
+	
+    // DAQ module configured
+    //SMU_LOGI(TAG,"[%1] module configured");
+
 	if (!ret)
 		state = State::STARTED;
 
 	return ret;
 }
+
+int SMUNode::stop()
+{
+    // Stop DAQ driver
+    if(!ioctl(fd, SMU_IOC_STOP, 0)){
+        // Unmap kernel-to-user shared memory
+        munmap(shared_mem, MMAP_SIZE);
+        while(close(fd) != 0){}
+    /*} else {
+        SMU_LOGE(TAG,"[%1] fail to stop the driver");
+        smuCFG->error.daq = SMU_ERR_DRV_NO_STOP;*/
+    }
+
+    // Stop Thread
+    /*SMU_LOGI(TAG,"[%1] module stopped");
+    smuCFG->error.daq = SMU_IDLE;
+    emit stopped();*/
+    //set_cpu_governor("ondemand");
+    return 1;
+}
+
+void SMUNode::sync_signal(int, siginfo_t *info, void*)
+{
+
+	//mem_pos = (info->si_value.sival_int);
+    int test = 5;
+    test ++;
+
+	/* Signal uldaq_read() about new data */
+	//pthread_cond_signal(&u->in.cv);
+}
+
+void SMUNode::data_available_signal(int, siginfo_t *info, void*)
+{
+
+	//mem_pos = (info->si_value.sival_int);
+    int test = 5;
+    test ++;
+
+	/* Signal uldaq_read() about new data */
+	//pthread_cond_signal(&u->in.cv);
+}
+
 
 int SMUNode::_read(struct Sample *smps[], unsigned cnt)
 {
@@ -57,7 +196,7 @@ int SMUNode::_read(struct Sample *smps[], unsigned cnt)
 	t->sequence = sequence++;
 	t->length = 1;
 	t->signals = in.signals;
-	sleep(1);
+	sleep(10);
 	for (unsigned i = 0; i < t->length; i++) {
 		t->data[i].f = 1.0;
 
