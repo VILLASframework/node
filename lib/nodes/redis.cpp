0aafb47dd21ddeaaa9cf7311f176486e634eb6d6
Steffen Vogel <steffen.vogel@opal-rt.com>
2023-04-03 13:04:21 +0000

    redis: fix compiler warnings
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- lib/nodes/redis.cpp
+++ lib/nodes/redis.cpp
@@ -216,7 +216,7 @@ void redis_on_message(NodeCompat *n, const std::string &channel, const std::stri
 
 	n->logger->debug("Message: {}: {}", channel, msg);
 
-	int alloc, scanned, pushed;
+	int alloc, scanned, pushed = 0;
 	unsigned cnt = n->in.vectorize;
 	struct Sample *smps[cnt];
 
@@ -247,7 +247,6 @@ void redis_on_message(NodeCompat *n, const std::string &channel, const std::stri
 
 	if (scanned < 0) {
 		n->logger->error("Failed to decode samples");
-		pushed = 0;
 		goto out;
 	}
 
