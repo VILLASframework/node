bca7f70cc2e25da1b91f237c98e0ad9966e76c55
Philipp Jungkamp <Philipp.Jungkamp@opal-rt.com>
2023-06-13 10:40:53 +0200

    packaging-nix: tailor libdatachannel compilation to our needs
    - Use libnice for better ICE protocol support.
    - Disable websockets support since we are already using libwebsockets.
    - Disable media transport facilities.
    
    Signed-off-by: Philipp Jungkamp <Philipp.Jungkamp@opal-rt.com>

--- packaging/nix/libdatachannel.nix
+++ packaging/nix/libdatachannel.nix
@@ -1,6 +1,9 @@
 {
   cmake,
   lib,
+  libnice,
+  libpcap,
+  pkg-config,
   stdenv,
   src,
   openssl,
@@ -9,8 +12,13 @@ stdenv.mkDerivation {
   pname = "libdatachannel";
   version = "villas";
   src = src;
-  nativeBuildInputs = [cmake];
-  buildInputs = [openssl];
+  nativeBuildInputs = [cmake pkg-config];
+  buildInputs = [libnice libpcap openssl];
+  cmakeFlags = [
+    "-DUSE_NICE=ON" # use libnice for better protocol support
+    "-DNO_WEBSOCKET=ON" # villas uses libwebsockets instead
+    "-DNO_MEDIA=ON" # villas does not use media transport features
+  ];
   meta = with lib; {
     description = "C/C++ WebRTC network library featuring Data Channels, Media Transport, and WebSockets";
     homepage = "https://libdatachannel.org/";
