ff2d24962f61721491e9e811e39e76ec1d9d9213
Philipp Jungkamp <Philipp.Jungkamp@opal-rt.com>
2023-09-20 16:17:38 +0200

    ci: Check Python code with black and mypy
    Signed-off-by: Philipp Jungkamp <Philipp.Jungkamp@opal-rt.com>

--- python/setup.py
+++
@@ -1,8 +0,0 @@
-"""
-SPDX-FileCopyrightText: 2014-2023 Institute for Automation of Complex Power Systems, RWTH Aachen University
-SPDX-License-Identifier: Apache-2.0
-"""  # noqa: E501
-
-from setuptools import setup
-
-setup()
