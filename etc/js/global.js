5d3b4bd4d64ad50c3259efdce8d979cdf438d112
Steffen Vogel <post@steffenvogel.de>
2024-02-29 21:47:29 +0100

    Use spaces for indention of Javacript files
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- etc/js/global.js
+++ etc/js/global.js
@@ -14,36 +14,36 @@ os = require('os');
 var logfile = "/var/log/villas-node_" + new Date().toISOString() + ".log"
 
 module.exports = {
-	affinity : 0x01,				// Mask of cores the server should run on
-							// This also maps the NIC interrupts to those cores!
+    affinity : 0x01,				// Mask of cores the server should run on
+                            // This also maps the NIC interrupts to those cores!
 
-	priority : 50,					// Priority for the server tasks.
-							// Usually the server is using a real-time FIFO
-							// scheduling algorithm
+    priority : 50,					// Priority for the server tasks.
+                            // Usually the server is using a real-time FIFO
+                            // scheduling algorithm
 
-							// See: https://github.com/docker/docker/issues/22380
-							//  on why we cant use real-time scheduling in Docker
+                            // See: https://github.com/docker/docker/issues/22380
+                            //  on why we cant use real-time scheduling in Docker
 
-	stats : 3,					// The interval in seconds to print path statistics.
-							// A value of 0 disables the statistics.
+    stats : 3,					// The interval in seconds to print path statistics.
+                            // A value of 0 disables the statistics.
 
-	name : os.hostname(),				// The name of this VILLASnode. Might by used by node-types
-							// to identify themselves (default is the hostname).
+    name : os.hostname(),				// The name of this VILLASnode. Might by used by node-types
+                            // to identify themselves (default is the hostname).
 
-	log : {
-		level : 5,				// The level of verbosity for debug messages
-							// Higher number => increased verbosity
+    log : {
+        level : 5,				// The level of verbosity for debug messages
+                            // Higher number => increased verbosity
 
-		faciltities : [ "path", "socket" ],	// The list of enabled debug faciltities.
-							// If omitted, all faciltities are enabled
-							// For a full list of available faciltities, check lib/log.c
+        faciltities : [ "path", "socket" ],	// The list of enabled debug faciltities.
+                            // If omitted, all faciltities are enabled
+                            // For a full list of available faciltities, check lib/log.c
 
-		file : logfile,				// File for logs
-	},
+        file : logfile,				// File for logs
+    },
 
-	http : {
-		enabled : true,				// Do not listen on port if true
+    http : {
+        enabled : true,				// Do not listen on port if true
 
-		port : 80				// Port for HTTP connections
-	}
+        port : 80				// Port for HTTP connections
+    }
 };
