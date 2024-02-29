1560f676567fa21eb438c02a64aa68e501f4dfc6
Steffen Vogel <post@steffenvogel.de>
2024-02-29 21:55:49 +0100

    Reformat Python code with black
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- etc/python/example.py
+++ etc/python/example.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
-''' Example Python config
+""" Example Python config
 
  This example demonstrates how you can use Python to generate complex
  configuration files.
@@ -11,7 +11,7 @@
  Author: Steffen Vogel <post@steffenvogel.de>
  SPDX-FileCopyrightText: 2014-2023 Institute for Automation of Complex Power Systems, RWTH Aachen University
  SPDX-License-Identifier: Apache-2.0
-'''
+"""
 
 import json
 import sys
@@ -19,59 +19,38 @@ import sys
 N = 10
 
 nodes = {
-    'raspberry': {
-        'type': 'socket',
-        'layer': 'udp',
-        'format': 'protobuf',
-
-        'in': {
-            'address': '*:12000',
+    "raspberry": {
+        "type": "socket",
+        "layer": "udp",
+        "format": "protobuf",
+        "in": {
+            "address": "*:12000",
         },
-        'out': {
-            'address': '1.2.3.4:12000'
-        }
+        "out": {"address": "1.2.3.4:12000"},
     }
 }
 
 for i in range(N):
-    name = f'agent{i}'
+    name = f"agent{i}"
     port = 12000 + i
 
     nodes[name] = {
-        'type': 'socket',
-        'layer': 'udp',
-        'format': 'protobuf',
-
-        'in': {
-            'address': '*:12000',
-            'signals': [
-                {
-                    'name': 'in',
-                    'type': 'float'
-                }
-            ]
-        },
-        'out': {
-            'address': f'5.6.7.8:{port}'
-        }
+        "type": "socket",
+        "layer": "udp",
+        "format": "protobuf",
+        "in": {"address": "*:12000", "signals": [{"name": "in", "type": "float"}]},
+        "out": {"address": f"5.6.7.8:{port}"},
     }
 
 paths = [
     {
-        'in': [f'agent{i}' for i in range(N)],
-        'out': 'raspberry',
-        'mode': 'any',
-        'hooks': [
-            {
-                'type': 'print'
-            }
-        ]
+        "in": [f"agent{i}" for i in range(N)],
+        "out": "raspberry",
+        "mode": "any",
+        "hooks": [{"type": "print"}],
     },
 ]
 
-config = {
-    'nodes': nodes,
-    'paths': paths
-}
+config = {"nodes": nodes, "paths": paths}
 
 json.dump(config, sys.stdout, indent=2)
