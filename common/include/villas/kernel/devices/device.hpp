babe80f53ec489a607fa8df1b652bac6fe4574fe
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2024-08-24 12:36:02 +0200

    add device interface
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

---
+++ common/include/villas/kernel/devices/device.hpp
@@ -0,0 +1,33 @@
+/* Device
+ *
+ * Author: Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ *
+ * SPDX-FileCopyrightText: 2023-2024 Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <filesystem>
+#include <optional>
+#include <villas/kernel/devices/driver.hpp>
+
+namespace villas {
+namespace kernel {
+namespace devices {
+
+class Device {
+public:
+  virtual ~Device(){};
+
+  virtual std::optional<std::unique_ptr<Driver>> driver() const = 0;
+  virtual std::optional<int> iommu_group() const = 0;
+  virtual std::string name() const = 0;
+  virtual std::filesystem::path override_path() const = 0;
+  virtual std::filesystem::path path() const = 0;
+  virtual void probe() const = 0;
+};
+
+} // namespace devices
+} // namespace kernel
+} // namespace villas
