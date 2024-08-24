d65910a14a8b040748bd6bcb744e57e91a0f108f
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2024-08-24 12:37:06 +0200

    add IpDevice
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

---
+++ common/include/villas/kernel/devices/ip_device.hpp
@@ -0,0 +1,34 @@
+/* IpDevice
+ *
+ * Author: Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ *
+ * SPDX-FileCopyrightText: 2023-2024 Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <filesystem>
+#include <villas/kernel/devices/platform_device.hpp>
+
+namespace villas {
+namespace kernel {
+namespace devices {
+
+class IpDevice : public PlatformDevice {
+public:
+public:
+  static IpDevice from(const std::filesystem::path unsafe_path);
+  static bool is_path_valid(const std::filesystem::path unsafe_path);
+
+private:
+  IpDevice(const std::filesystem::path valid_path) : PlatformDevice(valid_path){};
+
+public:
+  size_t addr() const;
+  std::string ip_name() const;
+};
+
+} // namespace devices
+} // namespace kernel
+} // namespace villas
