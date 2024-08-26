4e5f23ea66d21ec7df9a65f17cb552171b6f8b74
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2024-08-26 13:05:48 +0200

    add device ip matcher

---
+++ common/include/villas/kernel/devices/device_ip_matcher.hpp
@@ -0,0 +1,39 @@
+/* Devicematcher
+ *
+ * Author: Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ *
+ * SPDX-FileCopyrightText: 2023-2024 Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <villas/fpga/core.hpp>
+#include <villas/kernel/devices/ip_device.hpp>
+
+class DeviceIpMatcher {
+private:
+  std::vector<villas::kernel::devices::IpDevice> devices;
+  std::list<std::shared_ptr<villas::fpga::ip::Core>> ips;
+
+public:
+  DeviceIpMatcher(std::vector<villas::kernel::devices::IpDevice> devices,
+                  std::list<std::shared_ptr<villas::fpga::ip::Core>> ips)
+      : devices(devices), ips(ips) {}
+
+  std::vector<std::pair<std::shared_ptr<villas::fpga::ip::Core>,
+                        villas::kernel::devices::IpDevice>>
+  match() const {
+    std::vector<std::pair<std::shared_ptr<villas::fpga::ip::Core>,
+                          villas::kernel::devices::IpDevice>>
+        pairs;
+    for (auto device : devices) {
+      for (auto ip : ips) {
+        if (ip->getBaseaddr() == device.addr()) {
+          pairs.push_back(std::make_pair(ip, device));
+        }
+      }
+    }
+    return pairs;
+  }
+};
