1d071d01b048dba1ba3184866ee0be17341c4e28
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2024-09-20 17:12:35 +0200

    platform irq draft

--- fpga/include/villas/fpga/ips/intc.hpp
+++ fpga/include/villas/fpga/ips/intc.hpp
@@ -26,8 +26,8 @@ public:
   virtual bool init() override;
   virtual bool stop() override;
 
-  bool enableInterrupt(IrqMaskType mask, bool polling);
-  bool enableInterrupt(IrqPort irq, bool polling) {
+  virtual bool enableInterrupt(IrqMaskType mask, bool polling);
+  virtual bool enableInterrupt(IrqPort irq, bool polling) {
     return enableInterrupt(1 << irq.num, polling);
   }
 
@@ -37,7 +37,7 @@ public:
   ssize_t waitForInterrupt(int irq);
   ssize_t waitForInterrupt(IrqPort irq) { return waitForInterrupt(irq.num); }
 
-private:
+protected:
   static constexpr char registerMemory[] = "reg0";
 
   std::list<MemoryBlockName> getMemoryBlocks() const {
