80d794223c617cdba8cfabf137b697e3dba75102
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2024-09-23 17:12:16 +0200

    Refactor: move device utility functions to villas util
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

--- common/lib/utils.cpp
+++ common/lib/utils.cpp
@@ -17,7 +17,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <dirent.h>
 #include <fcntl.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <pthread.h>
 #include <unistd.h>
 
@@ -349,6 +353,37 @@ bool isPrivileged() {
   fclose(f);
 
   return true;
+}
+
+void write_to_file(std::string data, const std::filesystem::path file) {
+  villas::Log::get("Filewriter")->debug("{} > {}", data, file.u8string());
+  std::ofstream outputFile(file.u8string());
+
+  if (outputFile.is_open()) {
+    // Write to file
+    outputFile << data;
+    outputFile.close();
+  } else {
+    throw std::filesystem::filesystem_error("Cannot open outputfile",
+                                            std::error_code());
+  }
+}
+
+std::vector<std::string> read_names_in_directory(const std::string &name) {
+  DIR *directory = opendir(name.c_str());
+
+  struct dirent *dp;
+  std::vector<std::string> names;
+  dp = readdir(directory);
+  while (dp != NULL) {
+    auto name = std::string(dp->d_name);
+    if (name != "." && name != "..") {
+      names.push_back(name);
+    }
+    dp = readdir(directory);
+  }
+  closedir(directory);
+  return names;
 }
 
 } // namespace utils
