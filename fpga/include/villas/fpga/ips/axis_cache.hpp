49ab4e8ff210b0f294caf565b881e5174e1d24b9
Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
2024-10-27 10:37:31 +0100

    fpga: Add driver for new register interface of axis cache IP
    Signed-off-by: Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>

---
+++ fpga/include/villas/fpga/ips/axis_cache.hpp
@@ -0,0 +1,47 @@
+/* Driver for AXI Stream read cache. This module is used to lower latency of
+ * a DMA Scatter Gather engine's descriptor fetching. The driver allows for
+ * invalidating the cache.
+ *
+ * Author: Niklas Eiling <niklas.eiling@eonerc.rwth-aachen.de>
+ * SPDX-FileCopyrightText: 2024 Niklas Eiling
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <villas/fpga/node.hpp>
+
+namespace villas {
+namespace fpga {
+namespace ip {
+
+class AxisCache : public Node {
+public:
+  AxisCache();
+  virtual ~AxisCache();
+  virtual bool init() override;
+  virtual bool check() override;
+  void invalidate();
+
+protected:
+  const size_t registerNum = 1;
+  const size_t registerSize = 32;
+  static constexpr char registerMemory[] = "reg0";
+  std::list<MemoryBlockName> getMemoryBlocks() const override {
+    return {registerMemory};
+  }
+  void setRegister(size_t reg, uint32_t value);
+  uint32_t getRegister(size_t reg);
+  void resetRegister(size_t reg);
+  void resetAllRegisters();
+};
+
+} // namespace ip
+} // namespace fpga
+} // namespace villas
+
+#ifndef FMT_LEGACY_OSTREAM_FORMATTER
+template <>
+class fmt::formatter<villas::fpga::ip::AxisCache>
+    : public fmt::ostream_formatter {};
+#endif
