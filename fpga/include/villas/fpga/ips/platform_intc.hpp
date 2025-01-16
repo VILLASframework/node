ce3a1af5ac055b0d84359f2aed436d6c99a06bc7
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2025-01-16 01:07:30 +0100

    implement platform interrupt controller
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

--- fpga/include/villas/fpga/ips/platform_intc.hpp
+++ fpga/include/villas/fpga/ips/platform_intc.hpp
@@ -1,18 +1,51 @@
 #pragma once
 
 #include <villas/fpga/ips/intc.hpp>
+#include <villas/fpga/platform_card.hpp>
 #include <villas/kernel/vfio_device.hpp>
+
+namespace villas {
+namespace fpga {
+namespace ip {
 
 class PlatformInterruptController
     : public villas::fpga::ip::InterruptController {
 public:
-  PlatformInterruptController(
-      std::shared_ptr<villas::kernel::vfio::Device> vfioDevice) {
-    //TODO: efds
-    this->num_irqs = vfioDevice->platformInterruptInit(efds);
-  }
+  std::shared_ptr<villas::fpga::ip::Core> core;
+
+  PlatformInterruptController(std::shared_ptr<villas::fpga::ip::Core> core)
+      : core(core) {
+    logger = Log::get("PlatformIntc");
+    this->id = IpIdentifier("acs:software:platformIntc:1.0",
+                            "PlatformInterruptcontroller");
+  };
+
+  std::list<MemoryBlockName> getMemoryBlocks() const override { return {}; }
+
+  bool init() override {
+    std::shared_ptr<villas::kernel::vfio::Device> vfio_device;
+    auto platform_card = dynamic_cast<villas::fpga::PlatformCard *>(card);
+    for (auto core_connection : platform_card->core_connections) {
+      if (core_connection.ip->getBaseaddr() == core->getBaseaddr()) {
+        vfio_device = core_connection.vfio_connection.vfio_device;
+        logger->info("Enabled VFIO-Interrupt for {} with addr {:#x} ",
+                     core->getInstanceName(), core->getBaseaddr());
+      }
+    }
+
+    if (vfio_device == nullptr) {
+      logger->warn("Could not find Vfio device for ip: {}",
+                   core->getInstanceName());
+      return -1;
+    }
+
+    this->num_irqs = vfio_device->platformInterruptInit(efds);
+
+    return true;
+  };
 
   bool enableInterrupt(IrqMaskType mask, bool polling) override {
+    logger->debug("Enabling interrupt (platform)");
     for (int i = 0; i < num_irqs; i++) {
       if (mask & (1 << i))
         this->polling[i] = polling;
@@ -20,3 +53,7 @@ public:
     return true;
   }
 };
+
+} // namespace ip
+} // namespace fpga
+} // namespace villas
