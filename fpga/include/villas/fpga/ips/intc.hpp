372212d024aebc306ca61b5b311a30b31bed4c90
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2025-02-20 11:52:56 +0100

    TOKEN: SQ
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

--- fpga/include/villas/fpga/ips/intc.hpp
+++ fpga/include/villas/fpga/ips/intc.hpp
@@ -27,18 +27,18 @@ public:
   virtual bool init() override;
   virtual bool stop() override;
 
-  bool enableInterrupt(IrqMaskType mask, bool polling);
-  bool enableInterrupt(IrqPort irq, bool polling) {
+  virtual bool enableInterrupt(IrqMaskType mask, bool polling);
+  virtual bool enableInterrupt(IrqPort irq, bool polling) {
     return enableInterrupt(1 << irq.num, polling);
   }
 
   bool disableInterrupt(IrqMaskType mask);
   bool disableInterrupt(IrqPort irq) { return disableInterrupt(1 << irq.num); }
 
-  ssize_t waitForInterrupt(int irq);
+  virtual ssize_t waitForInterrupt(int irq);
   ssize_t waitForInterrupt(IrqPort irq) { return waitForInterrupt(irq.num); }
 
-private:
+protected:
   static constexpr char registerMemory[] = "reg0";
 
   std::shared_ptr<villas::kernel::vfio::Device> vfioDevice = nullptr;
