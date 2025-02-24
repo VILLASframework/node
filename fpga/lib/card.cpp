493d8c8c3c48ec431e73d92aaa7f735183f7d422
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2025-02-24 15:44:10 +0100

    refactor: Move loading of switch to function.
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

--- fpga/lib/card.cpp
+++ fpga/lib/card.cpp
@@ -10,6 +10,7 @@
 
 #include <villas/exceptions.hpp>
 #include <villas/fpga/card.hpp>
+#include <villas/fpga/node.hpp>
 
 using namespace villas;
 using namespace villas::fpga;
@@ -168,3 +169,46 @@ void CardFactory::loadIps(std::shared_ptr<Card> card, json_t *json_ips,
     throw ConfigError(json_ips, "node-config-fpga-ips",
                       "Cannot initialize IPs of FPGA card {}", card->name);
 };
+
+void CardFactory::loadSwitch(std::shared_ptr<Card> card, json_t *json_paths) {
+  // Additional static paths for AXI-Stream switch
+  json_error_t err;
+  if (json_paths != nullptr) {
+    if (not json_is_array(json_paths))
+      throw ConfigError(json_paths, err, "",
+                        "Switch path configuration must be an array");
+
+    size_t i;
+    json_t *json_path;
+    json_array_foreach(json_paths, i, json_path) {
+      const char *from, *to;
+      int reverse = 0;
+
+      int ret = json_unpack_ex(json_path, &err, 0, "{ s: s, s: s, s?: b }",
+                               "from", &from, "to", &to, "reverse", &reverse);
+      if (ret != 0)
+        throw ConfigError(json_path, err, "",
+                          "Cannot parse switch path config");
+
+      auto masterIpCore = card->lookupIp(from);
+      if (!masterIpCore)
+        throw ConfigError(json_path, "", "Unknown IP {}", from);
+
+      auto slaveIpCore = card->lookupIp(to);
+      if (!slaveIpCore)
+        throw ConfigError(json_path, "", "Unknown IP {}", to);
+
+      auto masterIpNode = std::dynamic_pointer_cast<ip::Node>(masterIpCore);
+      if (!masterIpNode)
+        throw ConfigError(json_path, "", "IP {} is not a streaming node", from);
+
+      auto slaveIpNode = std::dynamic_pointer_cast<ip::Node>(slaveIpCore);
+      if (!slaveIpNode)
+        throw ConfigError(json_path, "", "IP {} is not a streaming node", to);
+
+      if (not masterIpNode->connect(*slaveIpNode, reverse != 0))
+        throw ConfigError(json_path, "", "Failed to connect node {} to {}",
+                          from, to);
+    }
+  }
+}
