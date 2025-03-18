c7ccc3dc52cb09560af81b82bfe024acaa7be4c8
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2025-03-18 17:23:34 +0100

    chore: update comments and formatting
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

--- fpga/lib/core_connection.cpp
+++ fpga/lib/core_connection.cpp
@@ -17,9 +17,10 @@ CoreConnection::CoreConnection(
     : logger(villas::Log::get("CoreConnection")), ip(ip),
       device_connection(device_connection) {};
 
-CoreConnection CoreConnection::from(std::shared_ptr<villas::fpga::ip::Core> ip,
-     std::shared_ptr<kernel::vfio::Container> vfio_container) {
-  // Find matching dtr device for ip
+CoreConnection
+CoreConnection::from(std::shared_ptr<villas::fpga::ip::Core> ip,
+                     std::shared_ptr<kernel::vfio::Container> vfio_container) {
+  // Find matching OS device for ip
   const std::filesystem::path PLATFORM_DEVICES_DIRECTORY(
       "/sys/bus/platform/devices");
   std::vector<villas::kernel::devices::IpDevice> devices =
@@ -33,7 +34,7 @@ CoreConnection CoreConnection::from(std::shared_ptr<villas::fpga::ip::Core> ip,
 
   if (it_device == devices.end()) {
     throw(std::runtime_error(
-        "No device found with dtr adress matching ip baseadress."));
+        "No device found with address matching ip baseadress."));
   }
 
   return CoreConnection(ip, villas::kernel::devices::DeviceConnection::from(
@@ -45,6 +46,7 @@ void CoreConnection::add_to_memorygraph() {
 
   for (std::string memory_block : ip->getMemoryBlocks()) {
     auto &mm = MemoryManager::get();
+    // 16 bit width => 2^16 = 65536
     const size_t ip_mem_size = 65536;
 
     size_t srcVertexId =
@@ -55,6 +57,7 @@ void CoreConnection::add_to_memorygraph() {
     size_t targetVertexId;
     targetVertexId = mm.findAddressSpace(taget_address_space_name);
 
+    // Translation 0,0: No translation needs to be done to host vfio.
     mm.createMapping(0, 0, ip_mem_size, "vfio to ip", srcVertexId,
                      targetVertexId);
 
