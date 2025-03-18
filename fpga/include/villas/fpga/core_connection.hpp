822e5782d55582f237c9d85d99d39405afb3ded6
Pascal Bauer <pascal.bauer@rwth-aachen.de>
2025-03-18 17:14:24 +0100

    chore: move implementation to .cpp file
    Signed-off-by: Pascal Bauer <pascal.bauer@rwth-aachen.de>

--- fpga/include/villas/fpga/core_connection.hpp
+++ fpga/include/villas/fpga/core_connection.hpp
@@ -26,59 +26,14 @@ public:
 
 private:
   CoreConnection(std::shared_ptr<villas::fpga::ip::Core> ip,
-                 villas::kernel::devices::DeviceConnection device_connection)
-      : logger(villas::Log::get("CoreConnection")), ip(ip),
-        device_connection(device_connection){};
+                 villas::kernel::devices::DeviceConnection device_connection);
 
 public:
   static CoreConnection
   from(std::shared_ptr<villas::fpga::ip::Core> ip,
-       std::shared_ptr<kernel::vfio::Container> vfio_container) {
-    // Find matching dtr device for ip
-    const std::filesystem::path PLATFORM_DEVICES_DIRECTORY(
-        "/sys/bus/platform/devices");
-    std::vector<villas::kernel::devices::IpDevice> devices =
-        villas::kernel::devices::IpDevice::from_directory(
-            PLATFORM_DEVICES_DIRECTORY);
+       std::shared_ptr<kernel::vfio::Container> vfio_container);
 
-    auto it_device =
-        std::find_if(devices.begin(), devices.end(),
-                     [ip](villas::kernel::devices::IpDevice device) {
-                       return ip->getBaseaddr() == device.addr();
-                     });
-
-    if (it_device == devices.end()) {
-      throw(std::runtime_error(
-          "No device found with dtr adress matching ip baseadress."));
-    }
-
-    return CoreConnection(ip, villas::kernel::devices::DeviceConnection::from(
-                                  *it_device, vfio_container));
-  }
-
-  void add_to_memorygraph() {
-    device_connection.addToMemorygraph();
-
-    for (std::string memory_block : ip->getMemoryBlocks()) {
-      auto &mm = MemoryManager::get();
-      const size_t ip_mem_size = 65536;
-
-      size_t srcVertexId =
-          mm.findAddressSpace(device_connection.vfio_device->getName());
-
-      std::string taget_address_space_name =
-          ip->getInstanceName() + "/" + memory_block;
-      size_t targetVertexId;
-      targetVertexId = mm.findAddressSpace(taget_address_space_name);
-
-      mm.createMapping(0, 0, ip_mem_size, "vfio to ip", srcVertexId,
-                       targetVertexId);
-
-      logger->debug("Connect {} and {}",
-                    mm.getGraph().getVertex(srcVertexId)->name,
-                    mm.getGraph().getVertex(targetVertexId)->name);
-    }
-  }
+  void add_to_memorygraph();
 };
 
 } // namespace fpga
---
+++ fpga/lib/core_connection.cpp
@@ -0,0 +1,68 @@
+/* Memorygraph-connection to a fpga ip core.
+ *
+ * Author: Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ *
+ * SPDX-FileCopyrightText: 2024-25 Pascal Bauer <pascal.bauer@rwth-aachen.de>
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <villas/fpga/core_connection.hpp>
+
+namespace villas {
+namespace fpga {
+
+CoreConnection::CoreConnection(
+    std::shared_ptr<villas::fpga::ip::Core> ip,
+    villas::kernel::devices::DeviceConnection device_connection)
+    : logger(villas::Log::get("CoreConnection")), ip(ip),
+      device_connection(device_connection) {};
+
+CoreConnection CoreConnection::from(std::shared_ptr<villas::fpga::ip::Core> ip,
+     std::shared_ptr<kernel::vfio::Container> vfio_container) {
+  // Find matching dtr device for ip
+  const std::filesystem::path PLATFORM_DEVICES_DIRECTORY(
+      "/sys/bus/platform/devices");
+  std::vector<villas::kernel::devices::IpDevice> devices =
+      villas::kernel::devices::IpDevice::from_directory(
+          PLATFORM_DEVICES_DIRECTORY);
+
+  auto it_device = std::find_if(devices.begin(), devices.end(),
+                                [ip](villas::kernel::devices::IpDevice device) {
+                                  return ip->getBaseaddr() == device.addr();
+                                });
+
+  if (it_device == devices.end()) {
+    throw(std::runtime_error(
+        "No device found with dtr adress matching ip baseadress."));
+  }
+
+  return CoreConnection(ip, villas::kernel::devices::DeviceConnection::from(
+                                *it_device, vfio_container));
+}
+
+void CoreConnection::add_to_memorygraph() {
+  device_connection.addToMemorygraph();
+
+  for (std::string memory_block : ip->getMemoryBlocks()) {
+    auto &mm = MemoryManager::get();
+    const size_t ip_mem_size = 65536;
+
+    size_t srcVertexId =
+        mm.findAddressSpace(device_connection.vfio_device->getName());
+
+    std::string taget_address_space_name =
+        ip->getInstanceName() + "/" + memory_block;
+    size_t targetVertexId;
+    targetVertexId = mm.findAddressSpace(taget_address_space_name);
+
+    mm.createMapping(0, 0, ip_mem_size, "vfio to ip", srcVertexId,
+                     targetVertexId);
+
+    logger->debug("Connect {} and {}",
+                  mm.getGraph().getVertex(srcVertexId)->name,
+                  mm.getGraph().getVertex(targetVertexId)->name);
+  }
+}
+
+} // namespace fpga
+} // namespace villas
