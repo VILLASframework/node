e9f2c0a88ea05bcdc3a072de790c35c286c239a5
Jitpanu Maneeratpongsuk <jitpanu.maneeratpongsuk@rwth-aachen.de>
2025-04-10 14:51:16 +0000

    fix: Change to new node API
    Signed-off-by: Jitpanu Maneeratpongsuk <jitpanu.maneeratpongsuk@rwth-aachen.de>

--- include/villas/nodes/opendss.hpp
+++ include/villas/nodes/opendss.hpp
@@ -12,8 +12,8 @@
 #include <villas/node.hpp>
 #include <villas/node/config.hpp>
 #include <villas/timing.hpp>
-#include "villas/format.hpp"
-#include "villas/node_compat.hpp"
+#include <villas/format.hpp>
+#include <villas/node_compat.hpp>
 
 extern "C" int DSSI(int mode, int arg);
 extern "C" char* DSSPut_Command(char* myCmd);
@@ -42,49 +42,60 @@ namespace node {
 // Forward declarations
 struct Sample;
 
-enum ElementType {
-  generator,
-  load,
-  monitor,
-  isource
-};
+class OpenDSS : public Node {
+protected:
+  enum ElementType {
+    generator,
+    load,
+    monitor,
+    isource
+  };
 
-struct Element {
-  std::string name;      // Name of the element must be that same as declared in OpenDSS file
-  ElementType type;      // Type of the element
-  std::vector<int> mode; // Mode is set according to the function mode in opendss
-};
+  struct Element {
+    std::string name;      // Name of the element must be that same as declared in OpenDSS file
+    ElementType type;      // Type of the element
+    std::vector<int> mode; // Mode is set according to the function mode in OpenDSS
+  };
 
-struct opendss {
-  Format *formatter;
+  virtual int _read(struct Sample *smps[], unsigned cnt);
+
+  virtual int _write(struct Sample *smps[], unsigned cnt);
+
+  void parseData(json_t *json, bool in);
+
+  void getElementName(ElementType type, std::unordered_set<std::string> *set);
+
+  int extractMonitorData(struct Sample *const *smps);
+
+public:
+  OpenDSS(const uuid_t &id = {}, const std::string &name = "");
+
+  bool writing_turn;
   const char *path;
+  timespec ts;
+
+  std::string cmd_command;
+  char* cmd_result;
+
   std::vector<Element> dataIn;  // Vector of element to be written
   std::vector<std::string> monitor_name;
   std::unordered_set<std::string> load_set, gen_set, monitor_set, isource_set;  // Set of corresponding element type
-  bool writing_turn;
-  timespec ts;
+
   pthread_mutex_t mutex;
   pthread_cond_t cv;
 
+  virtual ~OpenDSS();
+
+  virtual int prepare();
+
+  virtual int parse(json_t *json);
+
+  virtual int check();
+
+  virtual int start();
+
+  virtual int stop();
 };
-
-int opendss_parse(NodeCompat *n, json_t *json);
-
-int opendss_prepare(NodeCompat *n);
-
-int opendss_init(NodeCompat *n);
-
-int opendss_start(NodeCompat *n);
-
-int opendss_read(NodeCompat *n, struct Sample *const smps[], unsigned cnt);
-
-int opendss_write(NodeCompat *n, struct Sample *const smps[], unsigned cnt);
-
-int opendss_type_start(SuperNode *sn);
-
-int opendss_stop(NodeCompat *n);
-
-int opendss_destroy(NodeCompat *n);
 
 } // namespace node
 } // namespace villas
