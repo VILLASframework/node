6b751ee01680651e9dcab1def785ee01b13bf3c2
Philipp Jungkamp <p.jungkamp@gmx.net>
2025-04-15 11:53:03 +0200

    cmake: Compile with ASan and UBSan

--- cmake/config/Debug.cmake
+++ cmake/config/Debug.cmake
@@ -3,3 +3,7 @@
 # Author: Steffen Vogel <post@steffenvogel.de>
 # SPDX-FileCopyrightText: 2014-2023 Institute for Automation of Complex Power Systems, RWTH Aachen University
 # SPDX-License-Identifier: Apache-2.0
+
+if(NOT DEFINED VILLAS_COMPILE_WITH_SANITIZERS)
+    set(VILLAS_COMPILE_WITH_SANITIZERS ON)
+endif()
