7d4ede4a9468fd424ef703380a8ee10d783dfd6f
Steffen Vogel <post@steffenvogel.de>
2025-04-24 08:14:42 +0200

    fix(api): Fix code-formatting
    Signed-off-by: Steffen Vogel <post@steffenvogel.de>

--- lib/api/requests/metrics.cpp
+++ lib/api/requests/metrics.cpp
@@ -5,8 +5,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <chrono>
 #include <jansson.h>
-#include<chrono>
 #include <villas/api/request.hpp>
 #include <villas/api/response.hpp>
 #include <villas/api/session.hpp>
@@ -21,13 +21,12 @@ namespace api {
 
 class MetricsRequest : public Request {
 private:
-std::unordered_map<Stats::Metric,std::string> metrics_subset = {
-    {Stats::Metric::SMPS_SKIPPED,"skipped"},
-    {Stats::Metric::OWD,"owd"},
-    {Stats::Metric::AGE,"age"},
-    {Stats::Metric::SIGNAL_COUNT,"signalcnt"},
-    {Stats::Metric::RTP_PKTS_LOST,"rtp_pkts_lost"}
-};
+  std::unordered_map<Stats::Metric, std::string> metrics_subset = {
+      {Stats::Metric::SMPS_SKIPPED, "skipped"},
+      {Stats::Metric::OWD, "owd"},
+      {Stats::Metric::AGE, "age"},
+      {Stats::Metric::SIGNAL_COUNT, "signalcnt"},
+      {Stats::Metric::RTP_PKTS_LOST, "rtp_pkts_lost"}};
 
 public:
   using Request::Request;
@@ -39,27 +38,31 @@ public:
     if (body != nullptr)
       throw BadRequest("Nodes endpoint does not accept any body data");
 
-    
     std::string text_res = "";
     NodeList node_list = session->getSuperNode()->getNodes();
-    for(Node* node: node_list){
+    for (Node *node : node_list) {
       auto stats = node->getStats();
-      if(!stats)
+      if (!stats)
         continue;
       std::string node_name = node->getNameShort();
-      for(auto& metric:metrics_subset){
+      for (auto &metric : metrics_subset) {
         Hist histogram = stats->getHistogram(metric.first);
-        std::string t =std::to_string(
-          std::chrono::duration_cast<std::chrono::milliseconds >(
-            std::chrono::system_clock::now().time_since_epoch()
-          ).count()
-        );
-        text_res+=metric.second+" {node=\""+node_name+"\" acc=\"last\"} "+std::to_string(histogram.getLast())+" "+t+"\n";
-        text_res+=metric.second+" {node=\""+node_name+"\" acc=\"total\"} "+std::to_string(histogram.getTotal())+" "+t+"\n";
+        std::string t = std::to_string(
+            std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now().time_since_epoch())
+                .count());
+
+        text_res += metric.second + " {node=\"" + node_name +
+                    "\" acc=\"last\"} " + std::to_string(histogram.getLast()) +
+                    " " + t + "\n";
+        text_res += metric.second + " {node=\"" + node_name +
+                    "\" acc=\"total\"} " +
+                    std::to_string(histogram.getTotal()) + " " + t + "\n";
       }
     }
 
-    return new Response(session,HTTP_STATUS_OK, "text/plain; charset=UTF-8", Buffer(text_res.c_str(),text_res.size()));
+    return new Response(session, HTTP_STATUS_OK, "text/plain; charset=UTF-8",
+                        Buffer(text_res.c_str(), text_res.size()));
   }
 };
 
