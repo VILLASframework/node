f9f0577440f339b2a56235ad70c23a6d36f88842
Steffen Vogel <steffen.vogel@opal-rt.com>
2025-09-01 08:44:11 +0200

    chore(nix): Update nixpkgs and remove obsolete patches
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- packaging/nix/patches.nix
+++ packaging/nix/patches.nix
@@ -4,23 +4,20 @@
 # This overlay contains patches to dependencies of villas-node.
 # It is only guaranteed to work for the locked version of nixpkgs,
 # future updates to upstream nixpkgs may make these obsolete.
-final: prev: let
+final: prev:
+let
   inherit (final) lib;
-in {
+in
+{
   libiec61850 = prev.libiec61850.overrideAttrs {
     patches = [ ./libiec61850_debug_r_session.patch ];
-    cmakeFlags = (prev.cmakeFlags or []) ++ [
+    cmakeFlags = (prev.cmakeFlags or [ ]) ++ [
       "-DCONFIG_USE_EXTERNAL_MBEDTLS_DYNLIB=ON"
       "-DCONFIG_EXTERNAL_MBEDTLS_DYNLIB_PATH=${final.mbedtls}/lib"
       "-DCONFIG_EXTERNAL_MBEDTLS_INCLUDE_PATH=${final.mbedtls}/include"
     ];
-    nativeBuildInputs = (prev.nativeBuildInputs or []) ++ [ final.buildPackages.cmake ];
+    nativeBuildInputs = (prev.nativeBuildInputs or [ ]) ++ [ final.buildPackages.cmake ];
     buildInputs = [ final.mbedtls ];
     separateDebugInfo = true;
-  };
-
-  lib60870 = prev.lib60870.overrideAttrs {
-    buildInputs = [ final.mbedtls ];
-    cmakeFlags = [ (lib.cmakeBool "WITH_MBEDTLS3" true) ];
   };
 }
