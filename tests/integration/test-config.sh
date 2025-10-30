978d3d42bedc0ce84a829edb04ed6b2907e89f9a
Steffen Vogel <steffen.vogel@opal-rt.com>
2025-10-30 09:06:38 +0100

    fix(tests): Exclude opal.orchestra config from test-config
    This is required as the standard builds of VILLASnode does not include support for the opal.orchestra node-type
    
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- tests/integration/test-config.sh
+++ tests/integration/test-config.sh
@@ -12,7 +12,7 @@ set -e
 CONFIGS=$(find ${SRCDIR}/etc/ -name '*.conf' -o -name '*.json')
 
 for CONFIG in ${CONFIGS}; do
-    if [ "$(basename ${CONFIG})" == "opal.conf" ] ||
+    if [ "$(basename ${CONFIG})" == "opal_orchestra.conf" ] ||
        [ "$(basename ${CONFIG})" == "fpga.conf" ] ||
        [ "$(basename ${CONFIG})" == "paths.conf" ] ||
        [ "$(basename ${CONFIG})" == "tricks.json" ] ||
