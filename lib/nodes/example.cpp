93b4f0360866b7db7ce962563c468d5e9f41f265
Steffen Vogel <steffen.vogel@opal-rt.com>
2025-11-11 12:26:03 +0000

    fix(node-example): Remove header for example node-type
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- lib/nodes/example.cpp
+++ lib/nodes/example.cpp
@@ -9,148 +9,159 @@
  */
 
 #include <villas/exceptions.hpp>
-#include <villas/node_compat.hpp>
-#include <villas/nodes/example.hpp>
 #include <villas/sample.hpp>
 #include <villas/super_node.hpp>
+#include <villas/timing.hpp>
 #include <villas/utils.hpp>
 
 using namespace villas;
-using namespace villas::node;
 using namespace villas::utils;
+using namespace villas::node;
 
-ExampleNode::ExampleNode(const uuid_t &id, const std::string &name)
+class ExampleNode : public Node {
+
+protected:
+  // Place any configuration and per-node state here
+
+  // Settings
+  int setting1;
+
+  std::string setting2;
+
+  // States
+  int state1;
+  struct timespec start_time;
+
+  int _read(struct Sample *smps[], unsigned cnt) override {
+    int read;
+    struct timespec now;
+
+    // TODO: Add implementation here. The following is just an example
+
+    assert(cnt >= 1 && smps[0]->capacity >= 1);
+
+    now = time_now();
+
+    smps[0]->data[0].f = time_delta(&now, &start_time);
+
+    /* Dont forget to set other flags in struct Sample::flags
+    * E.g. for sequence no, timestamps... */
+    smps[0]->flags = (int)SampleFlags::HAS_DATA;
+    smps[0]->signals = getInputSignals(false);
+
+    read = 1; // The number of samples read
+
+    return read;
+  }
+
+  int _write(struct Sample *smps[], unsigned cnt) override {
+    int written;
+
+    // TODO: Add implementation here.
+
+    written = 0; // The number of samples written
+
+    return written;
+  }
+
+public:
+  ExampleNode(const uuid_t &id = {}, const std::string &name = "")
     : Node(id, name), setting1(72), setting2("something"), state1(0) {}
 
-ExampleNode::~ExampleNode() {}
+  /* All of the following virtual-declared functions are optional.
+   * Have a look at node.hpp/node.cpp for the default behaviour.
+   */
 
-int ExampleNode::prepare() {
-  state1 = setting1;
+  virtual ~ExampleNode() { }
 
-  if (setting2 == "double")
-    state1 *= 2;
+  int prepare() override {
+    state1 = setting1;
 
-  return 0;
-}
+    if (setting2 == "double")
+      state1 *= 2;
 
-int ExampleNode::parse(json_t *json) {
-  // TODO: Add implementation here. The following is just an example
+    return 0;
+  }
 
-  const char *setting2_str = nullptr;
+  int parse(json_t *json) override {
+    // TODO: Add implementation here. The following is just an example
 
-  json_error_t err;
-  int ret = json_unpack_ex(json, &err, 0, "{ s?: i, s?: s }", "setting1",
-                           &setting1, "setting2", &setting2_str);
-  if (ret)
-    throw ConfigError(json, err, "node-config-node-example");
+    const char *setting2_str = nullptr;
 
-  if (setting2_str)
-    setting2 = setting2_str;
+    json_error_t err;
+    int ret = json_unpack_ex(json, &err, 0, "{ s?: i, s?: s }", "setting1",
+                            &setting1, "setting2", &setting2_str);
+    if (ret)
+      throw ConfigError(json, err, "node-config-node-example");
 
-  return 0;
-}
+    if (setting2_str)
+      setting2 = setting2_str;
 
-int ExampleNode::check() {
-  if (setting1 > 100 || setting1 < 0)
-    return -1;
+    return 0;
+  }
 
-  if (setting2.empty() || setting2.size() > 10)
-    return -1;
+  int check() override {
+    if (setting1 > 100 || setting1 < 0)
+      return -1;
 
-  return Node::check();
-}
+    if (setting2.empty() || setting2.size() > 10)
+      return -1;
 
-int ExampleNode::start() {
-  // TODO add implementation here
+    return Node::check();
+  }
 
-  start_time = time_now();
+  const std::string &getDetails() override {
+    details = fmt::format("setting1={}, setting2={}", setting1, setting2);
+    return details;
+  }
 
-  return 0;
-}
+  int start() override {
+    // TODO add implementation here
 
-// int ExampleNode::stop()
-// {
-// 	// TODO add implementation here
-// 	return 0;
-// }
+    start_time = time_now();
 
-// int ExampleNode::pause()
-// {
-// 	// TODO add implementation here
-// 	return 0;
-// }
+    return 0;
+  }
 
-// int ExampleNode::resume()
-// {
-// 	// TODO add implementation here
-// 	return 0;
-// }
+  // int ExampleNode::stop() override {
+  //   // TODO add implementation here
+  //   return 0;
+  // }
 
-// int ExampleNode::restart()
-// {
-// 	// TODO add implementation here
-// 	return 0;
-// }
+  // int ExampleNode::pause() override {
+  //   // TODO add implementation here
+  //   return 0;
+  // }
 
-// int ExampleNode::reverse()
-// {
-// 	// TODO add implementation here
-// 	return 0;
-// }
+  // int ExampleNode::resume() override {
+  //   // TODO add implementation here
+  //   return 0;
+  // }
 
-// std::vector<int> ExampleNode::getPollFDs()
-// {
-// 	// TODO add implementation here
-// 	return {};
-// }
+  // int ExampleNode::restart() override {
+  //   // TODO add implementation here
+  //   return 0;
+  // }
 
-// std::vector<int> ExampleNode::getNetemFDs()
-// {
-// 	// TODO add implementation here
-// 	return {};
-// }
+  // int ExampleNode::reverse() override {
+  //   // TODO add implementation here
+  //   return 0;
+  // }
 
-// struct villas::node::memory::Type * ExampleNode::getMemoryType()
-// {
-//	// TODO add implementation here
-// }
+  // std::vector<int> ExampleNode::getPollFDs() override {
+  //   // TODO add implementation here
+  //   return {};
+  // }
 
-const std::string &ExampleNode::getDetails() {
-  details = fmt::format("setting1={}, setting2={}", setting1, setting2);
-  return details;
-}
+  // std::vector<int> ExampleNode::getNetemFDs() override {
+  //   // TODO add implementation here
+  //   return {};
+  // }
 
-int ExampleNode::_read(struct Sample *smps[], unsigned cnt) {
-  int read;
-  struct timespec now;
-
-  // TODO: Add implementation here. The following is just an example
-
-  assert(cnt >= 1 && smps[0]->capacity >= 1);
-
-  now = time_now();
-
-  smps[0]->data[0].f = time_delta(&now, &start_time);
-
-  /* Dont forget to set other flags in struct Sample::flags
-   * E.g. for sequence no, timestamps... */
-  smps[0]->flags = (int)SampleFlags::HAS_DATA;
-  smps[0]->signals = getInputSignals(false);
-
-  read = 1; // The number of samples read
-
-  return read;
-}
-
-int ExampleNode::_write(struct Sample *smps[], unsigned cnt) {
-  int written;
-
-  // TODO: Add implementation here.
-
-  written = 0; // The number of samples written
-
-  return written;
-}
+  // struct villas::node::memory::Type * ExampleNode::getMemoryType() override {
+  //   // TODO add implementation here
+  // }
+};
 
 // Register node
 static char n[] = "example";
