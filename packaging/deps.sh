639029a7c8ae22094a3564a2df3381b247eac4e1
Jitpanu Maneeratpongsuk <jitpanu.maneeratpongsuk@rwth-aachen.de>
2026-01-05 20:17:27 +0000

    feat(node-gateway): Add installation of gRPC to docker file
    Add download and compile of reflection.proto for gRPC server reflection to deps.sh
    
    Signed-off-by: Jitpanu Maneeratpongsuk <jitpanu.maneeratpongsuk@rwth-aachen.de>

--- packaging/deps.sh
+++ packaging/deps.sh
@@ -612,6 +612,21 @@ if ! pkg-config "nlohmann_json" &&
     cmake --build . \
         --target install \
         --parallel ${PARALLEL}
+
+# Get and complie gRPC server reflection proto description file
+if ! find /usr/{local,}/include -name reflection.pb.cc | grep -q . &&
+    should_build "gRPC reflection" "For API gateway node-type"; then
+    mkdir -p gRPC_reflection
+    pushd gRPC_reflection
+    curl https://raw.githubusercontent.com/grpc/grpc/refs/heads/master/src/proto/grpc/reflection/v1alpha/reflection.proto -o reflection.proto
+    protoc  -I. \
+            --cpp_out=. \
+            --grpc_out=. \
+            --plugin=protoc-gen-grpc=/usr/bin/grpc_cpp_plugin \
+            reflection.proto
+    mkdir -p /usr/local/include/grpc/reflection/v1alpha
+    cp *.h /usr/local/include/grpc/reflection/v1alpha/
+    cp *.cc /usr/local/include/grpc/reflection/v1alpha/
     popd
 fi
 
