23619b0acad19067de0123841c4ad61c46edc11c
Steffen Vogel <steffen.vogel@opal-rt.com>
2026-08-07 23:31:32 +0200

    fix(lua): Correct SampleFlags bit-shift comments in test hook
    The flag comments were off by one (e.g. value 1 labelled '(1 << 1)') and
    NEW_SIMULATION (131072) was labelled '(1 << 16)' (it's 1 << 17) and
    duplicated NEW_FRAME's description.
    
    Signed-off-by: Steffen Vogel <steffen.vogel@opal-rt.com>

--- lua/hooks/test.lua
+++ lua/hooks/test.lua
@@ -15,14 +15,14 @@ Reason = {
 }
 
 SampleFlags = {
-	HAS_TS_ORIGIN   = 1,          -- "(1 <<  1)"    Include origin timestamp in output.
-	HAS_TS_RECEIVED = 2,          -- "(1 <<  2)"    Include receive timestamp in output.
-	HAS_OFFSET      = 4,          -- "(1 <<  3)"    Include offset (received - origin timestamp) in output.
-	HAS_SEQUENCE    = 8,          -- "(1 <<  4)"    Include sequence number in output.
-	HAS_DATA        = 16,         -- "(1 <<  5)"    Include values in output.
+	HAS_TS_ORIGIN   = 1,          -- "(1 <<  0)"    Include origin timestamp in output.
+	HAS_TS_RECEIVED = 2,          -- "(1 <<  1)"    Include receive timestamp in output.
+	HAS_OFFSET      = 4,          -- "(1 <<  2)"    Include offset (received - origin timestamp) in output.
+	HAS_SEQUENCE    = 8,          -- "(1 <<  3)"    Include sequence number in output.
+	HAS_DATA        = 16,         -- "(1 <<  4)"    Include values in output.
 
-	NEW_FRAME       = 65536,      -- "(1 << 16)"    This sample is the first of a new simulation case
-	NEW_SIMULATION  = 131072,     -- "(1 << 16)"    This sample is the first of a new simulation case
+	NEW_FRAME       = 65536,      -- "(1 << 16)"    This sample is the first of a new frame
+	NEW_SIMULATION  = 131072,     -- "(1 << 17)"    This sample is the first of a new simulation case
 
 	ALL             = 2147483647, -- "INT_MAX"      Enable all output options.
 }
